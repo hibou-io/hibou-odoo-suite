d64991e74b3e05253e6713bff62a16e07cc076a2
Jared Kipe <jared@hibou.io>
2018-04-30 08:15:31 -0700

    Adding all from 11.0 https://github.com/hibou-io/odoo-hr-payroll
    hr_payroll_holidays
    hr_payroll_input_name_report
    hr_payroll_input_report
    hr_payroll_payment
    hr_payroll_timesheet
    hr_payslip_line_date
    l10n_us_fl_hr_payroll
    l10n_us_hr_payroll
    l10n_us_mo_hr_payroll
    l10n_us_oh_hr_payroll
    l10n_us_va_hr_payroll

---
+++ l10n_us_hr_payroll/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'USA - Payroll',
+    'author': 'Hibou Corp. <hello@hibou.io>',
+    'license': 'AGPL-3',
+    'category': 'Localization',
+    'depends': ['hr_payroll'],
+    'version': '11.0.2017.0.0',
+    'description': """
+USA Payroll Rules.
+==================
+
+    * Contract W4 Filing Status & Allowances
+    * FICA Social Security (with wages cap)
+    * FICA Medicare
+    * FICA Additioal Medicare Wages & Tax
+    * FUTA Federal Unemployment (with wages cap)
+    * Federal Income Tax Withholdings based on W4 values
+    """,
+
+    'auto_install': False,
+    'website': 'https://hibou.io/',
+    'data': [
+        'l10n_us_hr_payroll_view.xml',
+        'data/base.xml',
+        'data/rules_2016.xml',
+        'data/rules_2017.xml',
+        'data/rules_2018.xml',
+        'data/final.xml',
+    ],
+    'installable': True
+}
---
+++ l10n_us_hr_payroll/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import test_us_payslip
+from . import test_us_payslip_2016
+from . import test_us_payslip_2017
+from . import test_us_payslip_2018
