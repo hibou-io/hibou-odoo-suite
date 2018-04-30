eb4de8bddcace5c2bc0e5f9da3485c8a55668319
Jared Kipe <jared@hibou.io>
2018-04-30 08:15:31 -0700

    Adding all from 11.0 https://github.com/hibou-io/odoo-hr-payroll
    hr_payroll_holidays
    hr_payroll_input_name_report
    hr_payroll_input_report
    hr_payroll_payment
    hr_payroll_timesheet
    hr_payslip_line_date
    l10n_us_fl_hr_payroll
    l10n_us_hr_payroll
    l10n_us_mo_hr_payroll
    l10n_us_oh_hr_payroll
    l10n_us_va_hr_payroll

---
+++ l10n_us_oh_hr_payroll/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'USA - Ohio - Payroll',
+    'author': 'Hibou Corp. <hello@hibou.io>',
+    'license': 'AGPL-3',
+    'category': 'Localization',
+    'depends': ['l10n_us_hr_payroll'],
+    'version': '11.0.2017.0.0',
+    'description': """
+USA::Ohio Payroll Rules.
+========================
+
+    * Ohio Department of Revenue partner
+    * Contribution register for Ohio DoR Unemployment
+    * Contribution register for Ohio DoR Income Tax Withholding
+    * Contract level Ohio Withholding Allowance
+    * Company level Ohio Unemployment Rate
+    """,
+
+    'auto_install': False,
+    'website': 'https://hibou.io/',
+    'data':[
+        'hr_payroll_view.xml',
+        'data/base.xml',
+        'data/rules_2016.xml',
+        'data/rules_2017.xml',
+        'data/rules_2018.xml',
+        'data/final.xml',
+    ],
+    'installable': True
+}
---
+++ l10n_us_oh_hr_payroll/tests/test_us_oh_payslip_2017.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+from odoo.addons.l10n_us_hr_payroll.tests.test_us_payslip import TestUsPayslip, process_payslip
+from odoo.addons.l10n_us_hr_payroll.l10n_us_hr_payroll import USHrContract
+
+
+class TestUsOhPayslip(TestUsPayslip):
+    ###
+    #    Taxes and Rates
+    ###
+    OH_UNEMP_MAX_WAGE = 9000.0
+
+    def test_2017_taxes(self):
+        salary = 5000.0
+
+        # For formula here
+        # http://www.tax.ohio.gov/Portals/0/employer_withholding/August2015Rates/WTH_OptionalComputerFormula_073015.pdf
+        tw = salary * 12  # = 60000
+        wd = ((tw - 40000) * 0.035 + 900) / 12 * 1.112
+
+        employee = self._createEmployee()
+        employee.company_id.oh_unemp_rate_2017 = 2.7
+
+        contract = self._createContract(employee, salary, struct_id=self.ref('l10n_us_oh_hr_payroll.hr_payroll_salary_structure_us_oh_employee'))
+
+        # tax rates
+        oh_unemp = contract.oh_unemp_rate(2017) / -100.0
+
+        self._log('2016 Ohio tax last payslip:')
+        payslip = self._createPayslip(employee, '2016-12-01', '2016-12-31')
+        payslip.compute_sheet()
+        process_payslip(payslip)
+
+        self._log('2017 Ohio tax first payslip:')
+        payslip = self._createPayslip(employee, '2017-01-01', '2017-01-31')
+
+        payslip.compute_sheet()
+
+        cats = self._getCategories(payslip)
+
+        self.assertPayrollEqual(cats['OH_UNEMP_WAGES'], salary)
+        self.assertPayrollEqual(cats['OH_UNEMP'], cats['OH_UNEMP_WAGES'] * oh_unemp)
+        self.assertPayrollEqual(cats['OH_INC_WITHHOLD'], -wd)
+
+        process_payslip(payslip)
+
+        # Make a new payslip, this one will have maximums
+
+        remaining_oh_unemp_wages = self.OH_UNEMP_MAX_WAGE - salary if (self.OH_UNEMP_MAX_WAGE - 2*salary < salary) \
+            else salary
+
+        self._log('2017 Ohio tax second payslip:')
+        payslip = self._createPayslip(employee, '2017-02-01', '2017-02-28')
+
+        payslip.compute_sheet()
+
+        cats = self._getCategories(payslip)
+
+        self.assertPayrollEqual(cats['OH_UNEMP_WAGES'], remaining_oh_unemp_wages)
+        self.assertPayrollEqual(cats['OH_UNEMP'], remaining_oh_unemp_wages * oh_unemp)
+
+    def test_2017_taxes_with_external(self):
+        salary = 5000.0
+        external_wages = 6000.0
+
+        employee = self._createEmployee()
+        employee.company_id.oh_unemp_rate_2017 = 2.8
+
+        contract = self._createContract(employee, salary, external_wages=external_wages,
+                                        struct_id=self.ref('l10n_us_oh_hr_payroll.hr_payroll_salary_structure_us_oh_employee'))
+
+        # tax rates
+        oh_unemp = contract.oh_unemp_rate(2017) / -100.0
+
+        self._log('2017 Ohio_external tax first payslip:')
+        payslip = self._createPayslip(employee, '2017-01-01', '2017-01-31')
+
+        payslip.compute_sheet()
+
+        cats = self._getCategories(payslip)
+
+        self.assertPayrollEqual(cats['OH_UNEMP_WAGES'], self.OH_UNEMP_MAX_WAGE - external_wages)
+        self.assertPayrollEqual(cats['OH_UNEMP'], cats['OH_UNEMP_WAGES'] * oh_unemp)
+
+    def test_2017_taxes_with_state_exempt(self):
+        salary = 5000.0
+        external_wages = 6000.0
+
+        employee = self._createEmployee()
+        employee.company_id.oh_unemp_rate_2017 = 2.9
+
+        contract = self._createContract(employee, salary, external_wages=external_wages, struct_id=self.ref(
+            'l10n_us_oh_hr_payroll.hr_payroll_salary_structure_us_oh_employee'), futa_type=USHrContract.FUTA_TYPE_BASIC)
+
+        # tax rates
+        oh_unemp = contract.oh_unemp_rate(2017) / -100.0
+
+        self.assertPayrollEqual(oh_unemp, 0.0)
+
+        self._log('2017 Ohio exempt tax first payslip:')
+        payslip = self._createPayslip(employee, '2017-01-01', '2017-01-31')
+
+        payslip.compute_sheet()
+
+        cats = self._getCategories(payslip)
+
+        self.assertPayrollEqual(cats['OH_UNEMP_WAGES'], self.OH_UNEMP_MAX_WAGE - external_wages)
+        self.assertPayrollEqual(cats['OH_UNEMP'], cats['OH_UNEMP_WAGES'] * oh_unemp)
