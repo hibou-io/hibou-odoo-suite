5667b7da7ac008b17566bb130e23a8b292f48d9a
Jared Kipe <jared@hibou.io>
2018-04-30 08:15:31 -0700

    Adding all from 11.0 https://github.com/hibou-io/odoo-hr-payroll
    hr_payroll_holidays
    hr_payroll_input_name_report
    hr_payroll_input_report
    hr_payroll_payment
    hr_payroll_timesheet
    hr_payslip_line_date
    l10n_us_fl_hr_payroll
    l10n_us_hr_payroll
    l10n_us_mo_hr_payroll
    l10n_us_oh_hr_payroll
    l10n_us_va_hr_payroll

---
+++ hr_payslip_line_date/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Date on Payslip Lines',
+    'author': 'Hibou Corp. <hello@hibou.io>',
+    'license': 'AGPL-3',
+    'category': 'Human Resources',
+    'depends': ['hr_payroll_account'],
+    'version': '11.0.0.0.0',
+    'description': """
+Date on Payslip Lines
+=====================
+
+    * Adds "Date Account" (date) field to payslip line from payslip
+    * Adds group by date to Payslip Line search view
+    * Allows filtering by "Date Account" for easy period reporting
+    """,
+
+    'auto_install': True,
+    'website': 'https://hibou.io/',
+    'data':[
+        'payslip_view.xml',
+    ],
+    'installable': True
+}
