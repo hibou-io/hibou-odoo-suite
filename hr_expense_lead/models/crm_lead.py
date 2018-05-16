92e33b16857c7c4663fae1d2a1349eef46b3db52
Jared Kipe <jared@hibou.io>
2018-05-16 10:08:45 -0700

    Initial commit of `hr_expense_lead` for 11.0

---
+++ hr_expense_lead/models/crm_lead.py
@@ -0,0 +1,14 @@
+from odoo import api, fields, models
+
+
+class CRMLead(models.Model):
+    _inherit = 'crm.lead'
+
+    expense_total_amount = fields.Float(string='Expenses Total', compute='_compute_expense_total_amount')
+    expense_ids = fields.One2many('hr.expense', 'lead_id', string='Expenses')
+
+    @api.multi
+    @api.depends('expense_ids.unit_amount')
+    def _compute_expense_total_amount(self):
+        for lead in self.sudo():
+            lead.expense_total_amount = sum(lead.expense_ids.mapped('total_amount'))
