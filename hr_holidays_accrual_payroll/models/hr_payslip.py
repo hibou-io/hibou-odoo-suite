a24b2fcb3a22eb7af49f577f61ec8271e8457250
Kristen Marie Kulha <kristen@hibou.io>
2018-08-09 11:24:13 -0700

    Added new `accrue_max` field to `hr_holidays_accrual` and `hr_holidays_accrual_payroll`.

--- hr_holidays_accrual_payroll/models/hr_payslip.py
+++ hr_holidays_accrual_payroll/models/hr_payslip.py
@@ -13,7 +13,20 @@ class HRPayslip(models.Model):
                                                ('accrue_by_pay_period', '=', True)])
             for leave_to_update in leaves_to_update:
                 new_allocation = leave_to_update.number_of_days_temp + leave_to_update.allocation_per_period
-                leave_to_update.write({'number_of_days_temp': new_allocation})
+                q = """SELECT SUM(number_of_days)
+                       FROM hr_holidays
+                       WHERE employee_id = %d AND holiday_status_id = %d;""" % (leave_to_update.employee_id.id, leave_to_update.holiday_status_id.id)
+                self.env.cr.execute(q)
+                total_days = self.env.cr.fetchall()
+                total_days = total_days[0][0]
+                new_total_days = total_days + leave_to_update.allocation_per_period
+                if leave_to_update.accrue_max and total_days > leave_to_update.accrue_max:
+                    new_allocation = leave_to_update.number_of_days_temp
+                elif leave_to_update.accrue_max and new_total_days > leave_to_update.accrue_max:
+                    difference = leave_to_update.accrue_max - total_days
+                    new_allocation = leave_to_update.number_of_days_temp + difference
+                if leave_to_update.number_of_days_temp != new_allocation:
+                    leave_to_update.write({'number_of_days_temp': new_allocation})
 
         return res
 
@@ -23,10 +36,12 @@ class HRHolidays(models.Model):
 
     accrue_by_pay_period = fields.Boolean(string="Accrue by Pay Period")
     allocation_per_period = fields.Float(string="Allocation Per Pay Period", digits=(12, 4))
+    accrue_max = fields.Float(string="Maximum Accrual")
 
     def _accrue_for_employee_values(self, employee):
         values = super(HRHolidays, self)._accrue_for_employee_values(employee)
         if values:
             values['accrue_by_pay_period'] = self.accrue_by_pay_period
             values['allocation_per_period'] = self.allocation_per_period
+            values['accrue_max'] = self.accrue_max
         return values
