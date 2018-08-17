06b043d8475ee08937e1a02ea65e7556330ef232
Jared Kipe <jared@hibou.io>
2018-08-17 14:42:53 -0700

    Initial commit of `rma` and `rma_sale` for Odoo 11.0
    Big changes to tests (because of the ways stock has changed) and allow activities to be set on RMAs.  Other refactors to reduce code duplication between picking and so returns.

---
+++ rma/__manifest__.py
@@ -0,0 +1,27 @@
+# © 2018 Hibou Corp.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Hibou RMAs',
+    'version': '11.0.1.0.0',
+    'category': 'Warehouse',
+    'author': "Hibou Corp.",
+    'license': 'AGPL-3',
+    'website': 'https://hibou.io/',
+    'depends': [
+        'stock',
+        'delivery',
+    ],
+    'data': [
+        'data/ir_sequence_data.xml',
+        'security/ir.model.access.csv',
+        'views/rma_views.xml',
+        'views/stock_picking_views.xml',
+        'wizard/rma_lines_views.xml',
+    ],
+    'demo': [
+        'data/rma_demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+ }
