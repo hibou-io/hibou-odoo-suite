5c15a702b98f9bec02d717eafe6e20be1e95809f
Jared Kipe <jared@jaredkipe.com>
2018-10-30 08:29:47 -0700

    Initial commit of `project_task_line` for 11.0

---
+++ project_task_line/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Project Task Lines',
+    'version': '11.0.1.0.0',
+    'author': 'Hibou Corp. <hello@hibou.io>',
+    'website': 'https://hibou.io/',
+    'license': 'AGPL-3',
+    'category': 'Tools',
+    'complexity': 'easy',
+    'description': """
+Adds "todo" lines onto Project Tasks, and improves sub-tasks.
+    """,
+    'depends': [
+        'project',
+    ],
+    'data': [
+        'views/project_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
