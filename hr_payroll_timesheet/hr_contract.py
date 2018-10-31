19fa0dba34bf33056bb4c8d1b911f915c51208e9
Jared Kipe <jared@jaredkipe.com>
2018-10-31 09:58:09 -0700

    Initial commit of *new* `hr_payroll_timesheet` and `hr_payroll_timesheet_old` for 11.0

--- hr_payroll_timesheet/hr_contract.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-
-class HrContract(models.Model):
-    _inherit = 'hr.contract'
-
-    paid_hourly = fields.Boolean(string="Paid Hourly", default=False)
