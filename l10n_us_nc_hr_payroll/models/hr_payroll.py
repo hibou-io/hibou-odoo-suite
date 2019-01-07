832f0d5ca59e86dac5de30c410ae9c708f04b3a7
Jared Kipe <jared@hibou.io>
2019-01-07 08:43:51 -0800

    Migrate `l10n_us_nc_hr_payroll` for 2019 and to use 	`hr.payroll.rate` instead of company field.

--- l10n_us_nc_hr_payroll/models/hr_payroll.py
+++ l10n_us_nc_hr_payroll/models/hr_payroll.py
@@ -15,21 +15,3 @@ class USNCHrContract(models.Model):
     nc_nc4_additional_wh = fields.Float(string="Additional Witholding", default=0.0,
                                         help="If you are filling out NC-4 NRA, this would be box 4; " \
                                              "otherwise box 2.")
-
-    @api.multi
-    def nc_unemp_rate(self, year):
-        self.ensure_one()
-        if self.futa_type == self.FUTA_TYPE_BASIC:
-            return 0.0
-
-        if hasattr(self.employee_id.company_id, 'nc_unemp_rate_' + str(year)):
-            return self.employee_id.company_id['nc_unemp_rate_' + str(year)]
-
-        raise NotImplemented('Year (' + str(year) + ') Not implemented for US North Carolina.')
-
-
-class NCCompany(models.Model):
-    _inherit = 'res.company'
-
-    # Unemployment rate provided by Katherine
-    nc_unemp_rate_2018 = fields.Float(string="North Carolina Unemployment Rate 2018", default=0.06)
