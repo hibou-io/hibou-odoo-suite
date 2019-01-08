edd3bbf19247e958e2bffa7c0563e95cca9fa217
Jared Kipe <jared@hibou.io>
2019-01-08 11:49:20 -0800

    Backport PA Payroll to 10.0 WIP

---
+++ l10n_us_pa_hr_payroll/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'USA - Pennsylvania - Payroll',
+    'author': 'Hibou Corp. <hello@hibou.io>',
+    'license': 'AGPL-3',
+    'category': 'Localization',
+    'depends': ['l10n_us_hr_payroll'],
+    'version': '11.0.2019.0.0',
+    'description': """
+USA::Pennsylvania Payroll Rules.
+================================
+
+* Partner for Pennsylvania Department of Revenue
+* Contribution register for Pennsylvania Department of Revenue - Unemployment Insurance
+* Contribution register Pennsylvania Department of Revenue - State Income Tax
+* Contract level Pennsylvania Unemployment Rate
+* Company level Pennsylvania Unemployement Rate
+    """,
+
+    'auto_install': False,
+    'website': 'https://hibou.io/',
+    'data': [
+        'views/hr_payroll_views.xml',
+        'data/base.xml',
+        'data/rates.xml',
+        'data/rules.xml',
+        'data/final.xml',
+    ],
+    'installable': True
+}
---
+++ l10n_us_pa_hr_payroll/tests/__init__.py
@@ -0,0 +1,2 @@
+from . import test_us_pa_payslip_2018
+from . import test_us_pa_payslip_2019
