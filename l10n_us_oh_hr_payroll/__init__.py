e2767269a7df196189d3f4aa43a74677edddc5ae
Jared Kipe <jared@hibou.io>
2019-01-08 09:45:59 -0800

    Backport 2019 OH Payroll WIP.

--- l10n_us_oh_hr_payroll/__init__.py
+++ l10n_us_oh_hr_payroll/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-import hr_payroll
+import models
--- l10n_us_oh_hr_payroll/tests/__init__.py
+++ l10n_us_oh_hr_payroll/tests/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import test_us_oh_payslip_2016
-from . import test_us_oh_payslip_2017
 from . import test_us_oh_payslip_2018
+from . import test_us_oh_payslip_2019
