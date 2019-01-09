58028e65b22d2fa9d0eb2bb3fbd8b3433d18d973
Jared Kipe <jared@hibou.io>
2019-01-08 19:32:44 -0800

    Deliberately not setting company_id in `hr_payroll_rate`
    This is intended to make instances of 'hr_payroll_rate' more like configuration for the modules that create them.
    Additional test and sorting makes it possible to 'prefer' to have company set to allow effectively per-company overrides.

--- hr_payroll_rate/models/payroll.py
+++ hr_payroll_rate/models/payroll.py
@@ -10,7 +10,7 @@ class PayrollRate(models.Model):
     date_from = fields.Date(string='Date From', required=True)
     date_to = fields.Date(string='Date To')
     company_id = fields.Many2one('res.company', string='Company', copy=False,
-                                 default=lambda self: self.env['res.company']._company_default_get())
+                                 default=False)
     rate = fields.Float(string='Rate', required=True)
     code = fields.Char(string='Code', required=True)
 
@@ -34,4 +34,4 @@ class Payslip(models.Model):
     def get_rate(self, code):
         self.ensure_one()
         return self.env['hr.payroll.rate'].search(
-            self._get_rate_domain(code), limit=1, order='date_from DESC')
+            self._get_rate_domain(code), limit=1, order='date_from DESC, company_id ASC')
