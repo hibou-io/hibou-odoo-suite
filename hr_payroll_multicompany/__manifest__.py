22e844cb58700452d67efbde2f37058fa6a59cf9
Jared Kipe <jared@jaredkipe.com>
2019-01-09 17:19:12 -0800

    Initial commit of `hr_payroll_multicompany` for 11.0
    This module is has been tested with a 5+ company database.  It implements accounting but the contribution registers are shared between companies.  (run as Admin)

---
+++ hr_payroll_multicompany/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Payroll Multicompany',
+    'description': 'Payroll Rates',
+    'version': '11.0.1.0.1',
+    'website': 'https://hibou.io/',
+    'author': 'Hibou Corp. <hello@hibou.io>',
+    'license': 'AGPL-3',
+    'category': 'Human Resources',
+    'data': [
+    ],
+    'depends': [
+        'hr_payroll_account',
+    ],
+}
