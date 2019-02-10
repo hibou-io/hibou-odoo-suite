a7c4f2028c2819bd947d3174d4d443b70277bc02
Jared Kipe <jared@hibou.io>
2019-02-10 13:20:09 -0800

    MIG `hr_payroll_rate` to 12.0

--- hr_payroll_rate/__manifest__.py
+++ hr_payroll_rate/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Payroll Rates',
     'description': 'Payroll Rates',
-    'version': '11.0.1.0.1',
+    'version': '12.0.1.0.1',
     'website': 'https://hibou.io/',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'license': 'AGPL-3',
