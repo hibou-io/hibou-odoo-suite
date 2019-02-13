16fe620f5ab071c902614a2b6a227535ad0f3229
Jared Kipe <jared@hibou.io>
2019-02-13 09:37:15 -0800

    Initial commit of `l10n_us_ms_hr_payroll` US Mississippi Payroll for 11.0

---
+++ l10n_us_ms_hr_payroll/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'USA - Mississippi - Payroll',
+    'author': 'Hibou Corp. <hello@hibou.io>',
+    'license': 'AGPL-3',
+    'category': 'Localization',
+    'depends': ['l10n_us_hr_payroll'],
+    'version': '11.0.2019.0.0',
+    'description': """
+USA::Mississippi Payroll Rules.
+===============================
+
+* New Partners and Contribution Registers for:
+     * Mississippi Department of Revenue
+     * Mississippi Department of Employment Security
+* Contract level Mississippi Exemptions and 89-350 fields
+* Payroll Rate for Mississippi Unemployment Rate
+    """,
+    'auto_install': False,
+    'website': 'https://hibou.io/',
+    'data': [
+        'views/hr_payroll_views.xml',
+        'data/base.xml',
+        'data/rates.xml',
+        'data/rules.xml',
+        'data/final.xml',
+    ],
+    'installable': True
+}
