1f4b9e1627b8911044863c2b731e9b0a423ef2f3
Jared Kipe <jared@hibou.io>
2019-02-13 07:53:55 -0800

    Initial commit of `l10n_us_mt_hr_payroll` US Montana Payroll for 11.0

---
+++ l10n_us_mt_hr_payroll/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'USA - Montana - Payroll',
+    'author': 'Hibou Corp. <hello@hibou.io>',
+    'license': 'AGPL-3',
+    'category': 'Localization',
+    'depends': ['l10n_us_hr_payroll'],
+    'version': '11.0.2019.0.0',
+    'description': """
+USA::Montana Payroll Rules.
+===========================
+
+* New Partners and Contribution Registers for:
+     * Montana Department of Revenue
+     * Montana Department of Labor & Industry
+* Contract level Montana Exemptions and MW-4 fields
+* Payroll Rate for Montana L&I Unemployment Rate
+    """,
+    'auto_install': False,
+    'website': 'https://hibou.io/',
+    'data': [
+        'views/hr_payroll_views.xml',
+        'data/base.xml',
+        'data/rates.xml',
+        'data/rules.xml',
+        'data/final.xml',
+    ],
+    'installable': True
+}
