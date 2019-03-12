7fadb31dd835465a7e3c090f7c4d067e8d9a77dd
Jared Kipe <jared@jaredkipe.com>
2019-03-12 07:18:49 -0700

    IMP `sale_line_reconfigure` add latest upstream changes.

--- sale_line_reconfigure/controllers/product_configurator.py
+++ sale_line_reconfigure/controllers/product_configurator.py
@@ -3,11 +3,12 @@ from odoo.addons.sale.controllers import product_configurator
 from odoo.http import request
 
 
-
 class ProductConfiguratorController(product_configurator.ProductConfiguratorController):
     @http.route(['/product_configurator/configure'], type='json', auth="user", methods=['POST'])
     def configure(self, product_id, pricelist_id, sale_line_id=None, **kw):
+        add_qty = int(kw.get('add_qty', 1))
         product_template = request.env['product.template'].browse(int(product_id))
+
         to_currency = product_template.currency_id
         pricelist = self._get_pricelist(pricelist_id)
         if pricelist:
@@ -19,13 +20,14 @@ class ProductConfiguratorController(product_configurator.ProductConfiguratorCont
             sale_line = request.env['sale.order.line'].browse(int(sale_line_id))
 
         return request.env['ir.ui.view'].render_template("sale.product_configurator_configure", {
+            'add_qty': add_qty,
             'product': product_template,
             'to_currency': to_currency,
             'pricelist': pricelist,
             'sale_line': sale_line,
+            # get_attribute_exclusions deprecated, use product method
             'get_attribute_exclusions': self._get_attribute_exclusions,
             'get_attribute_value_defaults': self._get_attribute_value_defaults,
-            'sale_line': sale_line,
         })
 
     def _get_attribute_value_defaults(self, product, sale_line, **kw):
