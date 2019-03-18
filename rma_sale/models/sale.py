d1a6334177b2fa748bb6c06fb04a65c1b3c0cc87
Jared Kipe <jared@jaredkipe.com>
2019-03-18 16:42:45 -0700

    IMP `rma_sale` Add SO Smart Button link to related RMAs.

---
+++ rma_sale/models/sale.py
@@ -0,0 +1,14 @@
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    rma_count = fields.Integer(string='RMA Count', compute='_compute_rma_count', compute_sudo=True)
+    rma_ids = fields.One2many('rma.rma', 'sale_order_id', string='RMAs')
+
+    @api.multi
+    @api.depends('rma_ids')
+    def _compute_rma_count(self):
+        for so in self:
+            so.rma_count = len(so.rma_ids)
