a0debba9d07556b7f1d9e7452f311b7fc3dce445
Jared Kipe <jared@jaredkipe.com>
2019-03-18 16:42:45 -0700

    IMP `rma_sale` Add SO Smart Button link to related RMAs.

--- rma_sale/__manifest__.py
+++ rma_sale/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Hibou RMAs for Sale Orders',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.1.0',
     'category': 'Sale',
     'author': "Hibou Corp.",
     'license': 'AGPL-3',
@@ -16,6 +16,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/rma_views.xml',
+        'views/sale_views.xml',
         'wizard/rma_lines_views.xml',
     ],
     'demo': [
