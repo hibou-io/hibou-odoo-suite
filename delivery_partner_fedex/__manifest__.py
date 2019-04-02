a004c3e35f3f00e3722f42dd1be191acae04ea27
Jared Kipe <jared@hibou.io>
2019-04-02 10:37:25 -0700

    MIG `delivery_partner_fedex` to 12.0

--- delivery_partner_fedex/__manifest__.py
+++ delivery_partner_fedex/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Fedex Partner Shipping Accounts',
     'author': 'Hibou Corp. <hello@hibou.io>',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Stock',
     'sequence': 95,
     'summary': 'Fedex Partner Shipping Accounts',
--- delivery_partner_fedex/models/delivery.py
+++ delivery_partner_fedex/models/delivery.py
@@ -10,7 +10,7 @@ class PartnerShippingAccount(models.Model):
     delivery_type = fields.Selection(selection_add=[('fedex', 'FedEx')])
 
     def fedex_check_validity(self):
-        m = re.search('^\d{9}$', self.name or '')
+        m = re.search(r'^\d{9}$', self.name or '')
         if not m:
             raise ValidationError('FedEx Account numbers must be 9 decimal numbers.')
 
