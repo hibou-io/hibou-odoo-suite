6223fe1641ec4fe4fc6665b4dc8fc2b23ef03f81
Jared Kipe <jared@hibou.io>
2019-04-04 11:18:00 -0700

    Initial commit of `project_stage` for 11.0

---
+++ project_stage/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Project Stages',
+    'version': '11.0.1.0.0',
+    'author': 'Hibou Corp. <hello@hibou.io>',
+    'website': 'https://hibou.io/',
+    'license': 'AGPL-3',
+    'category': 'Tools',
+    'complexity': 'easy',
+    'description': """
+Adds stages to Projects themselves.
+    """,
+    'depends': [
+        'project',
+    ],
+    'data': [
+        'views/project_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
