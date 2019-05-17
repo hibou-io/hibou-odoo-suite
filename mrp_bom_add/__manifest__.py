92d58a23bc8544abc3f22de17e3a356e95dcb6b6
Jared Kipe <jared@hibou.io>
2019-05-17 09:27:08 -0700

    IMP `mrp_bom_add` Optionally replace all existing lines on BoM for the selected product.

--- mrp_bom_add/__manifest__.py
+++ mrp_bom_add/__manifest__.py
@@ -2,14 +2,19 @@
     'name': 'BoM Mass Add',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'category': 'Hidden',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.1.0',
     'description':
         """
 Bill of Materials Mass Component Adder
 ======================================
 
 Helper to add all variants of a Product to a BoM filtered by the attributes on that product.
-Adds a button under BoM components named "Add Bulk".  This lets you configure a product
+Adds a button under BoM components named "Add Bulk".  This lets you select a product template 
+and add one BoM line for every variant of that product.
+
+Optionally you can choose to only add the variants that could be selected by the attributes 
+on the produced product itself.  You can also replace all of the lines for the slected product 
+to allow you to quickly re-configure qty or operation for bulk products.
         """,
     'depends': [
         'mrp',
