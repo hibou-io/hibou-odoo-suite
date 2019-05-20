88422b9c98116836b158c544558d2a0d4e234466
Jared Kipe <jared@hibou.io>
2019-05-20 15:54:00 -0700

    MIG `hr_payslip_line_date` to 12.0

--- hr_payslip_line_date/__manifest__.py
+++ hr_payslip_line_date/__manifest__.py
@@ -1,23 +1,22 @@
-# -*- encoding: utf-8 -*-
 {
     'name': 'Date on Payslip Lines',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'license': 'AGPL-3',
     'category': 'Human Resources',
     'depends': ['hr_payroll_account'],
-    'version': '11.0.0.0.0',
+    'version': '12.0.1.0.0',
     'description': """
 Date on Payslip Lines
 =====================
 
-    * Adds "Date Account" (date) field to payslip line from payslip
-    * Adds group by date to Payslip Line search view
-    * Allows filtering by "Date Account" for easy period reporting
+* Adds "Date Account" (date) field to payslip line from payslip
+* Adds group by date to Payslip Line search view
+* Allows filtering by "Date Account" for easy period reporting
     """,
 
     'auto_install': True,
     'website': 'https://hibou.io/',
-    'data':[
+    'data': [
         'payslip_view.xml',
     ],
     'installable': True
