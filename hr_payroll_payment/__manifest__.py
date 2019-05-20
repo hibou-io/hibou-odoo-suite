f76fa7c7a4e141cc373bab55c9d71beb5d870573
Jared Kipe <jared@hibou.io>
2019-05-20 13:56:05 -0700

    MIG `hr_payroll_payment` to 12.0.  Known issue where you have to change the payment journal to see the methods.

--- hr_payroll_payment/__manifest__.py
+++ hr_payroll_payment/__manifest__.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
-
 {
     'name': 'Payroll Payments',
     'author': 'Hibou Corp. <hello@hibou.io>',
-    'version': '11.0.0.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Human Resources',
     'sequence': 95,
     'summary': 'Register payments for Payroll Payslips',
