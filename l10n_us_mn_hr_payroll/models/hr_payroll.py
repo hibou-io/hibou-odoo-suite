3ee1d7fddaf6f0060ac39397812d5d6c9be04d08
David Frick <david@hibou.io>
2019-07-11 14:07:25 -0400

    NEW `l10n_us_mn_hr_payroll` for 11.0

---
+++ l10n_us_mn_hr_payroll/models/hr_payroll.py
@@ -0,0 +1,14 @@
+from odoo import models, fields, api
+
+
+class USMNHrContract(models.Model):
+    _inherit = 'hr.contract'
+
+    mn_w4mn_allowances = fields.Integer(string="MN Allowances")
+    mn_w4mn_additional_wh = fields.Float(string="MN Additional Withholding")
+    mn_w4mn_tax_exempt = fields.Boolean(string="MN Tax Exempt")
+    mn_w4mn_filing_status = fields.Selection([
+        # ('exempt', 'Exempt'),
+        ('single', 'Single'),
+        ('married', 'Married'),
+    ], string='MN Filing Status', default='single')
