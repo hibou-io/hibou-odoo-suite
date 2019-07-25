e177e6f2fd2374bedff17b7e37a7c01c289a64ce
Jared Kipe <jared@hibou.io>
2019-07-25 15:14:33 -0700

    IMP `delivery_partner_dhl` Allow 8-10 digit account numbers.

--- delivery_partner_dhl/models/delivery.py
+++ delivery_partner_dhl/models/delivery.py
@@ -10,8 +10,8 @@ class PartnerShippingAccount(models.Model):
     delivery_type = fields.Selection(selection_add=[('dhl', 'DHL')])
 
     def dhl_check_validity(self):
-        m = re.search(r'^\d{10}$', self.name or '')
+        m = re.search(r'^(\d{8}|\d{9}|\d{10})$', self.name or '')
         if not m:
-            raise ValidationError('DHL Account numbers must be 10 decimal numbers.')
+            raise ValidationError('DHL Account numbers must be 8-10 decimal numbers.')
 
 
