b8ea97c27bf579dfb06fe204898707d810ed12e1
Jared Kipe <jared@hibou.io>
2019-08-06 16:11:36 -0700

    FIX `sale_planner` It is possible that the 'fall back' core geo_localize can raise an index error

--- sale_planner/models/partner.py
+++ sale_planner/models/partner.py
@@ -13,17 +13,20 @@ class Partner(models.Model):
     def geo_localize(self):
         # We need country names in English below
         for partner in self.with_context(lang='en_US'):
-            if SearchEngine and partner.zip:
-                with SearchEngine() as search:
-                    zipcode = search.by_zipcode(str(self.zip).split('-')[0])
-                    if zipcode and zipcode.lat:
-                        partner.write({
-                            'partner_latitude': zipcode.lat,
-                            'partner_longitude': zipcode.lng,
-                            'date_localization': fields.Date.context_today(partner),
-                        })
-                    else:
-                        super(Partner, partner).geo_localize()
-            else:
-                super(Partner, partner).geo_localize()
+            try:
+                if SearchEngine and partner.zip:
+                    with SearchEngine() as search:
+                        zipcode = search.by_zipcode(str(self.zip).split('-')[0])
+                        if zipcode and zipcode.lat:
+                            partner.write({
+                                'partner_latitude': zipcode.lat,
+                                'partner_longitude': zipcode.lng,
+                                'date_localization': fields.Date.context_today(partner),
+                            })
+                        else:
+                            super(Partner, partner).geo_localize()
+                else:
+                    super(Partner, partner).geo_localize()
+            except:
+                pass
         return True
