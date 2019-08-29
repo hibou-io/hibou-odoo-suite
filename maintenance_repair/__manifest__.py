e40f5e4890b634de4cc655f1a232b1601c70e6bd
Bhoomi <bhoomi@hibou.io>
2019-08-29 13:14:30 -0400

    MIG `maintenance_repair` For Odoo 12.0

--- maintenance_repair/__manifest__.py
+++ maintenance_repair/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Equipment Repair',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'category': 'Human Resources',
     'summary': 'Consume products on Maintenance Requests',
