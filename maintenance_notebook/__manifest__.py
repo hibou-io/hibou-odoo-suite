d319a04d88d816921822077d47b3c77cdd49f058
Bhoomi <bhoomi@hibou.io>
2019-08-29 10:17:31 -0400

    MIG `maintenance_notebook` For Odoo 12.0

--- maintenance_notebook/__manifest__.py
+++ maintenance_notebook/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Maintenance Notebook',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'category': 'Human Resources',
     'summary': 'Record time on maintenance requests.',
