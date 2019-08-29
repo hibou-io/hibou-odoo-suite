58e4de8a370327d78ceb990c3707087e29ca9a6b
Bhoomi <bhoomi@hibou.io>
2019-08-29 10:44:45 -0400

    MIG `hr_department_project` For Odoo 12.0

--- hr_department_project/__manifest__.py
+++ hr_department_project/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'HR Department Project',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'category': 'Human Resources',
     'summary': 'Provide default project per Department',
