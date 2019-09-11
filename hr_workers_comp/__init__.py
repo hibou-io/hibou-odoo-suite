ecc12219e362d0d122a381ee1dde824336215366
Bhoomi <bhoomi@hibou.io>
2019-09-11 19:00:51 -0400

    MIG `hr_workers_comp` For Odoo 13.0

--- hr_workers_comp/__init__.py
+++ hr_workers_comp/__init__.py
@@ -1,1 +1,1 @@
-from . import contract
+from . import models
--- hr_workers_comp/__manifest__.py
+++ hr_workers_comp/__manifest__.py
@@ -3,20 +3,21 @@
     'author': 'Hibou Corp. <hello@hibou.io>',
     'license': 'AGPL-3',
     'category': 'Human Resources',
-    'depends': ['hr_contract'],
-    'version': '12.0.1.0.0',
+    'website': 'https://hibou.io/',
+    'version': '13.0.1.0.0',
     'description': """
 Workers' Compensation Class
 ===========================
 
 Provides a model to keep track of Workers' Comp. Class Codes and Rates.
     """,
-
-    'auto_install': False,
-    'website': 'https://hibou.io/',
-    'data':[
-        'ir.model.access.csv',
-        'contract_views.xml',
+    'depends': [
+        'hr_contract',
     ],
+    'data':[
+        'security/ir.model.access.csv',
+        'views/contract_views.xml',
+    ],
+    'auto_install': False,
     'installable': True
 }
