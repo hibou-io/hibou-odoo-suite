3d9939a6fc6042e885d799c1b0321ba9febc6791
Bhoomi <bhoomi@hibou.io>
2019-09-13 12:41:33 -0400

    MIG `maintenance_purchase_detail` For Odoo 12.0

--- maintenance_purchase_detail/__manifest__.py
+++ maintenance_purchase_detail/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Equipment Purchase Detail',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'category': 'Human Resources',
     'summary': 'Record purchase date and details on Equipments.',
