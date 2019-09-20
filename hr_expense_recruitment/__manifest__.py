ec35392db3898a6d8da7011dee041c593c707569
Bhoomi <bhoomi@hibou.io>
2019-09-20 11:30:29 -0400

    MIG `hr_expense_recruitment` For Odoo 12.0

--- hr_expense_recruitment/__manifest__.py
+++ hr_expense_recruitment/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'HR Expense Recruitment',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'category': 'Human Resources',
     'summary': 'Assign Recruitment to expenses for reporting.',
