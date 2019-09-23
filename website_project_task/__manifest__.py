7134c6f47af32ae1cf1525140afe52032d89e84d
Bhoomi <bhoomi@hibou.io>
2019-09-23 18:33:12 -0400

    MIG `website_project_task` For Odoo 13.0

--- website_project_task/__manifest__.py
+++ website_project_task/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Website Project Tasks',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'website': 'https://hibou.io/',
     'license': 'AGPL-3',
@@ -17,7 +17,7 @@ This module adds options to Website Project:
         'project',
     ],
     'data': [
-        'project_task_templates.xml',
+        'views/project_task_templates.xml',
     ],
     'installable': True,
     'auto_install': False,
