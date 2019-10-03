95c99d376431004bae854d3dbbad16129141ce91
Bhoomi <bhoomi@hibou.io>
2019-10-03 10:56:32 -0400

    MIG `sale_payment_deposit` For Odoo 13.0

--- sale_payment_deposit/models/sale.py
+++ sale_payment_deposit/models/sale.py
@@ -14,7 +14,6 @@ class SaleOrder(models.Model):
             flat_deposite = float(order.payment_term_id.deposit_flat)
             order.amount_total_deposit = percent_deposit + flat_deposite
 
-
     def action_confirm(self):
         res = super(SaleOrder, self).action_confirm()
         self._auto_deposit_invoice()
@@ -26,11 +25,11 @@ class SaleOrder(models.Model):
             # Create Deposit Invoices
             wizard = wizard_model.create({
                 'advance_payment_method': 'fixed',
-                'amount': sale.amount_total_deposit,
+                'fixed_amount': sale.amount_total_deposit,
             })
             wizard.with_context(active_ids=sale.ids).create_invoices()
             # Validate Invoices
-            sale.invoice_ids.filtered(lambda i: i.state == 'draft').action_invoice_open()
+            sale.invoice_ids.filtered(lambda i: i.state == 'draft').post()
             # Attempt to reconcile
             sale._auto_deposit_payment_match()
 
@@ -39,12 +38,12 @@ class SaleOrder(models.Model):
         # Note that this probably doesn't work for a payment made on the front, see .account.PaymentTransaction
         aml_model = self.env['account.move.line'].sudo()
         for sale in self.sudo():
-            for invoice in sale.invoice_ids.filtered(lambda i: i.state == 'open'):
-                outstanding = json_loads(invoice.outstanding_credits_debits_widget)
+            for invoice in sale.invoice_ids.filtered(lambda i: i.state == 'posted'):
+                outstanding = json_loads(invoice.invoice_outstanding_credits_debits_widget)
                 if isinstance(outstanding, dict) and outstanding.get('content'):
                     for line in outstanding.get('content'):
-                        if abs(line.get('amount', 0.0) - invoice.residual) < 0.01 and line.get('id'):
+                        if abs(line.get('amount', 0.0) - invoice.amount_residual) < 0.01 and line.get('id'):
                             aml = aml_model.browse(line.get('id'))
-                            aml += invoice.move_id.line_ids.filtered(lambda l: l.account_id == aml.account_id)
+                            aml += invoice.line_ids.filtered(lambda l: l.account_id == aml.account_id)
                             if aml.reconcile():
                                 break
