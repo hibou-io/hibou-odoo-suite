5c0e65ecdc6bc92cadb83e9f109deb05331463cf
Bhoomi <bhoomi@hibou.io>
2019-12-11 12:04:44 -0500

    MIG `maintenance_equipment_charge` For Odoo 12.0

--- maintenance_equipment_charge/__manifest__.py
+++ maintenance_equipment_charge/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Equipment Charges',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'category': 'Human Resources',
     'summary': 'Record related equipment charges.',
