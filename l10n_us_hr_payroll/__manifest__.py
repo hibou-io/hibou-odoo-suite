0bb2a9884cecbee01c1a5f2c12862aacb43a0b40
Jared Kipe <jared@hibou.io>
2020-01-09 13:07:03 -0800

    IMP `l10n_us_hr_payroll` Add MS Mississippi (unemployment, income tax)

--- l10n_us_hr_payroll/__manifest__.py
+++ l10n_us_hr_payroll/__manifest__.py
@@ -27,6 +27,7 @@ United States of America - Payroll Rules.
         'data/federal/fed_941_fit_rules.xml',
         'data/state/fl_florida.xml',
         'data/state/ga_georgia.xml',
+        'data/state/ms_mississippi.xml',
         'data/state/mt_montana.xml',
         'data/state/oh_ohio.xml',
         'data/state/pa_pennsylvania.xml',
