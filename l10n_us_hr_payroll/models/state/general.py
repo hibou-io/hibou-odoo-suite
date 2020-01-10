3ec63381b2be332d51add98cdcfee008e7e13cf6
Jared Kipe <jared@hibou.io>
2020-01-10 06:34:15 -0800

    IMP `l10n_us_hr_payroll` Refactor to simply tax exempt deductions.
    Additionally, it is possible (from testing on other databases) to have contracts without an employee, resulting in a failed migration.  Avoid this by filtering for employees on the contract and log if something (maybe a record rule) prevents you from seeing it instead of failing the migration itself.

--- l10n_us_hr_payroll/models/state/general.py
+++ l10n_us_hr_payroll/models/state/general.py
@@ -74,7 +74,7 @@ def _general_rate(payslip, wage, ytd_wage, wage_base=None, wage_start=None, rate
 def general_state_unemployment(payslip, categories, worked_days, inputs, wage_base=None, wage_start=None, rate=None, state_code=None):
     """
     Returns SUTA eligible wage and rate.
-    WAGE = GROSS - WAGE_US_940_FUTA_EXEMPT
+    WAGE = GROSS + DED_FUTA_EXEMPT
 
     The contract's `futa_type` determines if SUTA should be collected.
 
@@ -91,17 +91,17 @@ def general_state_unemployment(payslip, categories, worked_days, inputs, wage_ba
     # Determine Wage
     year = payslip.dict.get_year()
     ytd_wage = payslip.sum_category('GROSS', str(year) + '-01-01', str(year + 1) + '-01-01')
-    ytd_wage -= payslip.sum_category('WAGE_US_940_FUTA_EXEMPT', str(year) + '-01-01', str(year + 1) + '-01-01')
+    ytd_wage += payslip.sum_category('DED_FUTA_EXEMPT', str(year) + '-01-01', str(year + 1) + '-01-01')
     ytd_wage += payslip.dict.contract_id.external_wages
 
-    wage = categories.GROSS - categories.WAGE_US_940_FUTA_EXEMPT
+    wage = categories.GROSS + categories.DED_FUTA_EXEMPT
     return _general_rate(payslip, wage, ytd_wage, wage_base=wage_base, wage_start=wage_start, rate=rate)
 
 
 def general_state_income_withholding(payslip, categories, worked_days, inputs, wage_base=None, wage_start=None, rate=None, state_code=None):
     """
     Returns SIT eligible wage and rate.
-    WAGE = GROSS - WAGE_US_941_FIT_EXEMPT
+    WAGE = GROSS + DED_FIT_EXEMPT
 
     :return: result, result_rate (wage, percent)
     """
@@ -114,10 +114,10 @@ def general_state_income_withholding(payslip, categories, worked_days, inputs, w
     # Determine Wage
     year = payslip.dict.get_year()
     ytd_wage = payslip.sum_category('GROSS', str(year) + '-01-01', str(year + 1) + '-01-01')
-    ytd_wage -= payslip.sum_category('WAGE_US_941_FIT_EXEMPT', str(year) + '-01-01', str(year + 1) + '-01-01')
+    ytd_wage += payslip.sum_category('DED_FIT_EXEMPT', str(year) + '-01-01', str(year + 1) + '-01-01')
     ytd_wage += payslip.dict.contract_id.external_wages
 
-    wage = categories.GROSS - categories.WAGE_US_941_FIT_EXEMPT
+    wage = categories.GROSS + categories.DED_FIT_EXEMPT
     result, result_rate = _general_rate(payslip, wage, ytd_wage, wage_base=wage_base, wage_start=wage_start, rate=rate)
     additional = payslip.dict.contract_id.us_payroll_config_value('state_income_tax_additional_withholding')
     if additional:
--- l10n_us_hr_payroll/models/state/mt_montana.py
+++ l10n_us_hr_payroll/models/state/mt_montana.py
@@ -6,7 +6,7 @@ from .general import _state_applies
 def mt_montana_state_income_withholding(payslip, categories, worked_days, inputs):
     """
     Returns SIT eligible wage and rate.
-    WAGE = GROSS - WAGE_US_941_FIT_EXEMPT
+    WAGE = GROSS + DED_FIT_EXEMPT
 
     :return: result, result_rate (wage, percent)
     """
@@ -18,7 +18,7 @@ def mt_montana_state_income_withholding(payslip, categories, worked_days, inputs
         return 0.0, 0.0
 
     # Determine Wage
-    wage = categories.GROSS - categories.WAGE_US_941_FIT_EXEMPT
+    wage = categories.GROSS + categories.DED_FIT_EXEMPT
     schedule_pay = payslip.dict.contract_id.schedule_pay
     additional = payslip.dict.contract_id.us_payroll_config_value('state_income_tax_additional_withholding')
     exemptions = payslip.dict.contract_id.us_payroll_config_value('mt_mw4_sit_exemptions')
