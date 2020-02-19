f5f73f352cf7ead174dcf6ecf95ed723bda39de9
Jared Kipe <jared@hibou.io>
2020-02-19 12:04:03 -0800

    IMP `hr_payroll_slip_ytd` Add totals for Quantity and Amount for better YTD reporting.

--- hr_payroll_slip_ytd/models/payslip.py
+++ hr_payroll_slip_ytd/models/payslip.py
@@ -9,11 +9,20 @@ class Payslip(models.Model):
         from_date = str(self.date_to.year) + '-01-01'
         state_allowed = ('done', 'verify') if not allow_draft else ('done', 'verify', 'draft')
         self.env.cr.execute("""
-            SELECT sum(total) as sum
+            SELECT sum(total) as total,
+                   sum(quantity) as quantity,
+                   sum(amount) as amount
             FROM hr_payslip as hp
             JOIN hr_payslip_line as pi ON hp.id = pi.slip_id
             WHERE hp.employee_id = %s 
                   AND hp.state in %s
                   AND hp.date_to >= %s AND hp.date_to <= %s AND pi.code = %s""",
                             (self.employee_id.id, state_allowed, from_date, to_date, code))
-        return self.env.cr.fetchone()[0] or 0.0
+        res = self.env.cr.dictfetchone()
+        if res:
+            # Can return dictionary with NULL aka Nones
+            for key in res:
+                res[key] = res[key] or 0.0
+        else:
+            res = {'total': 0.0, 'quantity': 0.0, 'amount': 0.0}
+        return res
