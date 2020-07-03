5b7043dccf7f2c0c14488287525c06b43bd33a0e
Jared Kipe <jared@hibou.io>
2020-07-03 08:42:56 -0700

    [MOV] hr_payroll_commission: from Hibou Suite Enterprise for 13.0

---
+++ hr_payroll_commission/__manifest__.py
@@ -0,0 +1,29 @@
+# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
+
+{
+    'name': 'Commissions in Payslips',
+    'author': 'Hibou Corp.',
+    'version': '13.0.1.0.0',
+    'license': 'OPL-1',
+    'category': 'Accounting/Commissions',
+    'sequence': 95,
+    'summary': 'Reimburse Commissions in Payslips',
+    'description': """
+Reimburse Commissions in Payslips
+    """,
+    'depends': [
+        'hr_commission',
+        'hr_payroll',
+    ],
+    'data': [
+        'views/hr_commission_views.xml',
+        'views/hr_payslip_views.xml',
+        'data/hr_payroll_commission_data.xml',
+    ],
+    'demo': [
+        'data/hr_payroll_commission_demo.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
