ac73aa9f07cda7f48090bfeabce33c2fc26562f6
Jared Kipe <jared@hibou.io>
2020-07-07 14:42:42 -0700

    [MIG] hr_payroll_attendance: to Odoo 13.0 + [ADD] Overtime calculations

--- hr_payroll_attendance/__manifest__.py
+++ hr_payroll_attendance/__manifest__.py
@@ -1,16 +1,19 @@
 {
     'name': 'Attendance on Payslips',
     'description': 'Get Attendence numbers onto Employee Payslips.',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'website': 'https://hibou.io/',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'license': 'AGPL-3',
     'category': 'Human Resources',
     'data': [
-        'hr_contract_view.xml',
+        'data/hr_payroll_attendance_data.xml',
+        'views/hr_contract_views.xml',
+        'views/hr_payslip_views.xml',
     ],
     'depends': [
         'hr_payroll',
         'hr_attendance',
+        'hr_payroll_overtime',
     ],
 }
--- hr_payroll_attendance/models/hr_contract.py
+++ hr_payroll_attendance/models/hr_contract.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 class HrContract(models.Model):
     _inherit = 'hr.contract'
 
-    paid_hourly_attendance = fields.Boolean(string="Paid Hourly Attendance", default=False)
+    paid_hourly_attendance = fields.Boolean(string="Paid Hourly Attendance")
