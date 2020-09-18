46f595fafae2c33ce3f8ee7d32ba2a12a607d328
Brett Spaulding <brett@hibou.io>
2020-09-18 17:26:50 -0400

    [IMP] hr_attendance_work_entry:  Modified kiosk mode views and attendance views to allow for various punch types.

--- hr_attendance_work_entry/models/work_entry.py
+++ hr_attendance_work_entry/models/work_entry.py
@@ -3,6 +3,7 @@ from odoo import fields, models
 
 class HrWorkEntryType(models.Model):
     _inherit = 'hr.work.entry.type'
+    _order = 'sequence, id'
 
     allow_attendance = fields.Boolean(string='Allow in Attendance')
     attendance_icon = fields.Char(string='Attendance Icon', default='fa-sign-in')
@@ -10,4 +11,5 @@ class HrWorkEntryType(models.Model):
         # ('checked_out', "Checked out"),  # reserved for detecting new punch in
         ('checked_in', "Checked in"),
         ('break', 'Break'),
+        ('lunch', 'Lunch'),
     ], string='Attendance State', default='checked_in')
