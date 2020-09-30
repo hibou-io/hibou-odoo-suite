fde826ac02efb745e5f643f34067f42f6b2a2646
Jared Kipe <jared@hibou.io>
2020-09-30 11:08:30 -0700

    [ADD] hibou_professional: initial commit for 13.0

---
+++ hibou_professional/__manifest__.py
@@ -0,0 +1,24 @@
+# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
+
+{
+    'name': 'Hibou Professional',
+    'author': 'Hibou Corp. <hello@hibou.io>',
+    'category': 'Tools',
+    'depends': ['mail'],
+    'version': '13.0.1.0.0',
+    'description': """
+Hibou Professional Support and Billing
+======================================
+
+    """,
+    'website': 'https://hibou.io/',
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+    'qweb': [
+        'static/src/xml/templates.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'OPL-1',
+}
