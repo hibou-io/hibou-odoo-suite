8310c9981507f384381276b842e6e1d78c17d182
Jared Kipe <jared@hibou.io>
2020-10-05 13:05:51 -0700

    [IMP] hr_payroll_overtime: refactor calculation to allow 'recursion' or overtime on overtime
    Example.  Lets say you have 8hr/day overtime at 1.5x, and 12hr/day overtime at 2x.
    Now you can create a 2x overtime rules for 12 hours/day, and use it as the overtime rules for the original overtime worktype.

--- hr_payroll_overtime/tests/test_overtime.py
+++ hr_payroll_overtime/tests/test_overtime.py
@@ -269,3 +269,55 @@ class TestOvertime(common.TransactionCase):
     def test_11_overtime_aggregation_daily_week_start(self):
         self.employee.resource_calendar_id.day_week_start = '7'
         self.test_10_overtime_aggregation_daily()
+
+    def test_12_recursive_daily(self):
+        # recursive will use a second overtime
+        self.work_type_overtime2 = self.env['hr.work.entry.type'].create({
+            'name': 'Test Overtime 2',
+            'code': 'TEST_OT2'
+        })
+        self.overtime_rules2 = self.env['hr.work.entry.overtime.type'].create({
+            'name': 'Test2',
+            'hours_per_week': 999.0,
+            'hours_per_day': 12.0,
+            'multiplier': 2.0,
+        })
+        self.overtime_rules.hours_per_day = 8.0
+        self.overtime_rules.multiplier_per_day = 1.5
+        self.work_type_overtime.overtime_type_id = self.overtime_rules2
+        self.work_type_overtime.overtime_work_type_id = self.work_type_overtime2
+
+        work_data = [
+            ((2020, 24, 1), [
+                # regular day
+                (self.work_type, 4.0, None),
+                (self.work_type, 4.0, None),
+            ]),
+            ((2020, 24, 2), [
+                # 2hr overtime
+                (self.work_type, 4.0, None),
+                (self.work_type, 6.0, None),
+            ]),
+            ((2020, 24, 3), [
+                # 4hr overtime
+                (self.work_type, 6.0, None),
+                (self.work_type, 6.0, None),
+            ]),
+            ((2020, 24, 4), [
+                # 4hr overtime
+                # 2hr overtime2
+                (self.work_type, 6.0, None),
+                (self.work_type, 8.0, None),
+            ]),
+        ]
+
+        result_data = self.payslip.aggregate_overtime(work_data)
+        self.assertTrue(self.work_type in result_data)
+        self.assertEqual(result_data[self.work_type][0], 4)
+        self.assertEqual(result_data[self.work_type][1], 32.0)
+        self.assertTrue(self.work_type_overtime in result_data)
+        self.assertEqual(result_data[self.work_type_overtime][0], 0)
+        self.assertEqual(result_data[self.work_type_overtime][1], 10.0)
+        self.assertTrue(self.work_type_overtime2 in result_data)
+        self.assertEqual(result_data[self.work_type_overtime2][0], 0)
+        self.assertEqual(result_data[self.work_type_overtime2][1], 2.0)
