afa035840b7f22ef4a269a45358f4bbe5002bb69
Jared Kipe <jared@hibou.io>
2020-10-08 17:00:08 -0700

    [ADD] account_section_subtotal: for Odoo 13.0

---
+++ account_section_subtotal/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Account Section Subtotal',
+    'version': '13.0.1.0.0',
+    'category': 'Accounting',
+    'author': 'Hibou Corp.',
+    'license': 'AGPL-3',
+    'website': 'https://hibou.io/',
+    'depends': [
+        'account',
+        'base_section_subtotal',
+    ],
+    'data': [
+        'views/account_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+ }
