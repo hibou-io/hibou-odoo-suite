6d430216ccc69ae135978b818fd3f06f917e6537
Leighton Pennicott <Leighton@hibou.io>
2020-11-09 09:56:12 -0500

    [MIG] delivery_partner_fedex: migrate to odoo 14.0
    Added 'ondelete' option. It's now required when the selection_add parameter is used.

--- delivery_partner_fedex/models/delivery.py
+++ delivery_partner_fedex/models/delivery.py
@@ -6,12 +6,10 @@ from odoo.exceptions import ValidationError
 
 class PartnerShippingAccount(models.Model):
     _inherit = 'partner.shipping.account'
-
-    delivery_type = fields.Selection(selection_add=[('fedex', 'FedEx')])
-
+    
+    delivery_type = fields.Selection(selection_add=[('fedex', 'FedEx')], ondelete={'fedex': 'set default'})
+    
     def fedex_check_validity(self):
         m = re.search(r'^\d{9}$', self.name or '')
         if not m:
             raise ValidationError('FedEx Account numbers must be 9 decimal numbers.')
-
-
