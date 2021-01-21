7d69d3eecd7c57311bce05ba0fd9159361707e47
Jared Self <jself@hibou.io>
2021-01-21 10:10:19 -0700

    TX Texas Amended

--- l10n_us_hr_payroll/tests/test_us_tx_texas_payslip_2021.py
+++ l10n_us_hr_payroll/tests/test_us_tx_texas_payslip_2021.py
@@ -5,13 +5,13 @@ from .common import TestUsPayslip
 
 class TestUsTXPayslip(TestUsPayslip):
     ###
-    #   2020 Taxes and Rates
+    #   2021 Taxes and Rates
     ###
     TX_UNEMP_MAX_WAGE = 9000.0
     TX_UNEMP = 2.7
     TX_OA = 0.0
     TX_ETIA = 0.1
 
-    def test_2020_taxes(self):
+    def test_2021_taxes(self):
         combined_rate = self.TX_UNEMP + self.TX_OA + self.TX_ETIA
-        self._test_er_suta('TX', combined_rate, date(2020, 1, 1), wage_base=self.TX_UNEMP_MAX_WAGE)
+        self._test_er_suta('TX', combined_rate, date(2021, 1, 1), wage_base=self.TX_UNEMP_MAX_WAGE)
