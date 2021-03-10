04ff9309901af1ec47b24de8451c7935c3355e51
Jared Kipe <jared@hibou.io>
2021-03-09 17:02:13 -0800

    [IMP] hr_payroll_hibou: add module `hr_payroll_payment`

--- hr_payroll_hibou/models/res_config_settings.py
+++ hr_payroll_hibou/models/res_config_settings.py
@@ -7,6 +7,7 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     # TODO We need MORE here...
+    module_hr_payroll_payment = fields.Boolean(string='Payments & Advanced Accounting')
     module_hr_payroll_attendance = fields.Boolean(string='Attendance Entries & Overtime')
     module_hr_payroll_timesheet = fields.Boolean(string='Timesheet Entries & Overtime')
     module_l10n_us_hr_payroll = fields.Boolean(string='USA Payroll')
