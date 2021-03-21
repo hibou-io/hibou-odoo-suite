df1da5ca56a4cae1aba3b1362fa9c6be90192d40
Jared Kipe <jared@hibou.io>
2021-03-21 15:28:39 -0700

    [MIG] timesheet_description_sale: for Odoo 14.0

--- timesheet_description_sale/__manifest__.py
+++ timesheet_description_sale/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Timesheet Description Sale',
-    'version': '13.0.1.0.0',
+    'version': '14.0.1.0.0',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'website': 'https://hibou.io/',
     'license': 'AGPL-3',
