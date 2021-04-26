6235c4a903e6f431fe316d6249c87cd67b17562b
Jared Kipe <jared@hibou.io>
2021-04-26 08:34:28 -0700

    [IMP] website_sale_signifyd: add tests and streamline setup (WIP)

--- website_sale_signifyd/controllers/main.py
+++ website_sale_signifyd/controllers/main.py
@@ -1,14 +1,19 @@
 from odoo.http import request, route
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
+import logging
+_logger = logging.getLogger(__name__)
+
 
 class WebsiteSale(WebsiteSale):
 
     @route(['/shop/confirmation'], type='http', auth="public", website=True, sitemap=False)
     def payment_confirmation(self, **post):
         res = super(WebsiteSale, self).payment_confirmation()
-        order_session_id = request.session.session_token
+        # order_session_id = request.session.session_token
         checkout_token = request.session.session_token
+        order_session_id = checkout_token  # TODO what is the appropriate variable?
+        _logger.warn(str(request.session))
         browser_ip_address = request.httprequest.environ['REMOTE_ADDR']
         sale_order_id = request.session.get('sale_last_order_id')
         if sale_order_id:
@@ -16,6 +21,7 @@ class WebsiteSale(WebsiteSale):
             # Post completed order to Signifyd
             signifyd = request.env.company.signifyd_connector_id
             if signifyd:
-                order.sudo().post_signifyd_case(order_session_id, checkout_token, browser_ip_address)
+                # TODO should the signifyd variable be used?
+                order.post_signifyd_case(order_session_id, checkout_token, browser_ip_address)
 
         return res
