f1386af073b290cb780624d0bcd016768e4097cf
Jared Kipe <jared@hibou.io>
2021-04-29 16:19:10 -0700

    [MIG] account_payment_disperse: Odoo 13.  Add new 'next due' behavior.

--- account_payment_disperse/tests/test_payment_multi.py
+++ account_payment_disperse/tests/test_payment_multi.py
@@ -1,8 +1,17 @@
+# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
+
 from odoo.addons.account.tests.test_payment import TestPayment
+from odoo.tests.common import Form
+from odoo.tests import tagged
 from odoo.exceptions import ValidationError
 import time
 
 
+# Fun fact... if you install enterprise accounting, you'll get errors
+# due to required fields being missing...
+# The classic fix would be the following @tagged, but for some reason this makes
+# odoo.tests.common.Form suddenly not calculate the amount on the register payment wizard
+# @tagged('post_install', '-at_install')
 class PaymentMultiTest(TestPayment):
     
     def test_multiple_payments_partial(self):
@@ -12,146 +21,143 @@ class PaymentMultiTest(TestPayment):
         inv_2 = self.create_invoice(amount=500, currency_id=self.currency_eur_id, partner=self.partner_agrolait.id)
 
         ids = [inv_1.id, inv_2.id]
-        register_payments = self.register_payments_model.with_context(active_ids=ids).create({
-            'payment_date': time.strftime('%Y') + '-07-15',
-            'journal_id': self.bank_journal_euro.id,
-            'payment_method_id': self.payment_method_manual_in.id,
-        })
-        register_payments.amount = 399.0
-        register_payments.is_manual_disperse = True
+        payment_register = Form(self.register_payments_model.with_context(active_ids=ids))
+        payment_register.journal_id = self.bank_journal_euro
+        payment_register.payment_date = time.strftime('%Y') + '-07-15'
+        payment_register.is_manual_disperse = True
 
-        with self.assertRaises(ValidationError):
-            register_payments.create_payments()
+        with payment_register.payment_invoice_ids.edit(0) as f:
+            f.amount = 99.0
+            f.save()
+        with payment_register.payment_invoice_ids.edit(1) as f:
+            f.amount = 300.0
+            f.save()
 
-        for line in register_payments.invoice_line_ids:
-            if line.invoice_id == inv_1:
-                line.amount = 99.0
-            if line.invoice_id == inv_2:
-                line.amount = 300.0
+        self.assertEqual(payment_register.amount, 399.0, 'Amount isn\'t the amount from lines')
 
-        register_payments.create_payments()
+        # Persist object
+        payment_register = payment_register.save()
+        payment_register.create_payments()
 
         payment_ids = self.payment_model.search([('invoice_ids', 'in', ids)], order="id desc")
         self.assertEqual(len(payment_ids), 1, 'Need only one payment.')
         self.assertEqual(payment_ids.amount, 399.0)
 
-        self.assertEqual(inv_1.residual_signed, 1.0)
-        self.assertEqual(inv_2.residual_signed, 200.0)
+        self.assertEqual(inv_1.amount_residual_signed, 1.0)
+        self.assertEqual(inv_2.amount_residual_signed, 200.0)
 
-        register_payments = self.register_payments_model.with_context(active_ids=ids).create({
-            'payment_date': time.strftime('%Y') + '-07-15',
-            'journal_id': self.bank_journal_euro.id,
-            'payment_method_id': self.payment_method_manual_in.id,
-        })
-        register_payments.amount = 200.0
-        register_payments.is_manual_disperse = True
+        payment_register = Form(self.register_payments_model.with_context(active_ids=ids))
+        payment_register.journal_id = self.bank_journal_euro
+        payment_register.payment_date = time.strftime('%Y') + '-07-15'
+        payment_register.is_manual_disperse = True
 
-        for line in register_payments.invoice_line_ids:
-            if line.invoice_id == inv_2:
-                line.amount = 200.0
-
-        register_payments.create_payments()
-        self.assertEqual(inv_1.residual_signed, 1.0)
-        self.assertEqual(inv_2.residual_signed, 0.0)
+        with payment_register.payment_invoice_ids.edit(0) as f:
+            f.amount = 0.0
+            f.save()
+        with payment_register.payment_invoice_ids.edit(1) as f:
+            f.amount = 200.0
+            f.save()
+        payment_register = payment_register.save()
+        payment_register.create_payments()
+        self.assertEqual(inv_1.amount_residual_signed, 1.0)
+        self.assertEqual(inv_2.amount_residual_signed, 0.0)
 
     def test_multiple_payments_write_off(self):
         inv_1 = self.create_invoice(amount=100, currency_id=self.currency_eur_id, partner=self.partner_agrolait.id)
         inv_2 = self.create_invoice(amount=500, currency_id=self.currency_eur_id, partner=self.partner_agrolait.id)
 
         ids = [inv_1.id, inv_2.id]
-        register_payments = self.register_payments_model.with_context(active_ids=ids).create({
-            'payment_date': time.strftime('%Y') + '-07-15',
-            'journal_id': self.bank_journal_euro.id,
-            'payment_method_id': self.payment_method_manual_in.id,
-        })
-        register_payments.amount = 400.0
-        register_payments.is_manual_disperse = True
-        register_payments.writeoff_journal_id = inv_1.journal_id
+        payment_register = Form(self.register_payments_model.with_context(active_ids=ids))
+        payment_register.journal_id = self.bank_journal_euro
+        payment_register.payment_date = time.strftime('%Y') + '-07-15'
+        payment_register.is_manual_disperse = True
+        payment_register.writeoff_account_id = self.transfer_account
 
-        with self.assertRaises(ValidationError):
-            register_payments.create_payments()
+        with payment_register.payment_invoice_ids.edit(0) as f:
+            f.amount = 100.0
+            f.close_balance = True
+            f.save()
+        with payment_register.payment_invoice_ids.edit(1) as f:
+            f.amount = 300.0
+            f.close_balance = True
+            f.save()
 
-        for line in register_payments.invoice_line_ids:
-            if line.invoice_id == inv_1:
-                line.amount = 100.0
-            if line.invoice_id == inv_2:
-                line.amount = 300.0
-                line.writeoff_acc_id = self.account_revenue
+        self.assertEqual(payment_register.amount, 400.0, 'Amount isn\'t the amount from lines')
 
-        register_payments.create_payments()
+        payment_register = payment_register.save()
+        self.assertEqual(sum(payment_register.mapped('payment_invoice_ids.difference')), -200.0)
+        payment_register.create_payments()
 
         payment_ids = self.payment_model.search([('invoice_ids', 'in', ids)], order="id desc")
         self.assertEqual(len(payment_ids), 1, 'Need only one payment.')
         self.assertEqual(payment_ids.amount, 400.0)
 
-        self.assertEqual(inv_1.residual_signed, 0.0)
-        self.assertEqual(inv_2.residual_signed, 0.0)
+        self.assertEqual(inv_1.amount_residual_signed, 0.0)
+        self.assertEqual(inv_2.amount_residual_signed, 0.0)
 
     def test_multiple_payments_partial_multi(self):
         """ Create test to pay several vendor bills/invoices at once """
-        # One payment for inv_1 and inv_2 (same partner)
+        # One payment for inv_1 and inv_2 (different partners)
         inv_1 = self.create_invoice(amount=100, currency_id=self.currency_eur_id, partner=self.partner_agrolait.id)
         inv_2 = self.create_invoice(amount=500, currency_id=self.currency_eur_id, partner=self.partner_china_exp.id)
 
         ids = [inv_1.id, inv_2.id]
-        register_payments = self.register_payments_model.with_context(active_ids=ids).create({
-            'payment_date': time.strftime('%Y') + '-07-15',
-            'journal_id': self.bank_journal_euro.id,
-            'payment_method_id': self.payment_method_manual_in.id,
-        })
-        register_payments.amount = 400.0
-        register_payments.is_manual_disperse = True
+        payment_register = Form(self.register_payments_model.with_context(active_ids=ids))
+        payment_register.journal_id = self.bank_journal_euro
+        payment_register.payment_date = time.strftime('%Y') + '-07-15'
+        payment_register.is_manual_disperse = True
 
-        for line in register_payments.invoice_line_ids:
-            if line.invoice_id == inv_1:
-                line.amount = 100.0
-            if line.invoice_id == inv_2:
-                line.amount = 300.0
+        with payment_register.payment_invoice_ids.edit(0) as f:
+            f.amount = 100.0
+            f.save()
+        with payment_register.payment_invoice_ids.edit(1) as f:
+            f.amount = 300.0
+            f.save()
 
-        register_payments.create_payments()
+        self.assertEqual(payment_register.amount, 400.0, 'Amount isn\'t the amount from lines')
+
+        payment_register = payment_register.save()
+        payment_register.create_payments()
 
         payment_ids = self.payment_model.search([('invoice_ids', 'in', ids)], order="id desc")
         self.assertEqual(len(payment_ids), 2, 'Need two payments.')
-        # Useful for logging amounts of payments and their accounting
-        # for pay in payment_ids:
-        #     _logger.warn(str(pay) + ' amount: ' + str(pay.amount))
-        #     for line in pay.move_line_ids:
-        #         _logger.warn('  ' +
-        #                      str(line) + ' name: ' + str(line.name) + ' :: credit: ' + str(line.credit) + ' debit: ' +
-        #                      str(line.debit))
         self.assertEqual(sum(payment_ids.mapped('amount')), 400.0)
 
-        self.assertEqual(inv_1.residual_signed, 0.0)
-        self.assertEqual(inv_2.residual_signed, 200.0)
+        self.assertEqual(inv_1.amount_residual_signed, 0.0)
+        self.assertEqual(inv_2.amount_residual_signed, 200.0)
 
     def test_vendor_multiple_payments_write_off(self):
         inv_1 = self.create_invoice(amount=100, type='in_invoice', currency_id=self.currency_eur_id, partner=self.partner_agrolait.id)
         inv_2 = self.create_invoice(amount=500, type='in_invoice', currency_id=self.currency_eur_id, partner=self.partner_agrolait.id)
         ids = [inv_1.id, inv_2.id]
-        register_payments = self.register_payments_model.with_context(active_ids=ids).create({
-            'payment_date': time.strftime('%Y') + '-07-15',
-            'journal_id': self.bank_journal_euro.id,
-            'payment_method_id': self.payment_method_manual_out.id,
-        })
-        register_payments.amount = 400.0
-        register_payments.is_manual_disperse = True
-        register_payments.writeoff_journal_id = inv_1.journal_id
+        payment_register = Form(self.register_payments_model.with_context(active_ids=ids))
+        payment_register.journal_id = self.bank_journal_euro
+        payment_register.payment_date = time.strftime('%Y') + '-07-15'
+        payment_register.is_manual_disperse = True
+
+        with payment_register.payment_invoice_ids.edit(0) as f:
+            f.amount = 100.0
+            f.save()
+        with payment_register.payment_invoice_ids.edit(1) as f:
+            f.amount = 300.0
+            f.save()
+
+        self.assertEqual(payment_register.amount, 400.0)
+
+        # Cannot have close balance in form because it becomes required
+        payment_register = payment_register.save()
+        payment_register.action_toggle_close_balance()
 
         with self.assertRaises(ValidationError):
-            register_payments.create_payments()
+            payment_register.create_payments()
 
-        for line in register_payments.invoice_line_ids:
-            if line.invoice_id == inv_1:
-                line.amount = 100.0
-            if line.invoice_id == inv_2:
-                line.amount = 300.0
-                line.writeoff_acc_id = self.account_revenue
-
-        register_payments.create_payments()
+        # Need the writeoff account
+        payment_register.writeoff_account_id = self.transfer_account
+        payment_register.create_payments()
 
         payment_ids = self.payment_model.search([('invoice_ids', 'in', ids)], order="id desc")
         self.assertEqual(len(payment_ids), 1, 'Need only one payment.')
         self.assertEqual(payment_ids.amount, 400.0)
 
-        self.assertEqual(inv_1.residual_signed, 0.0)
-        self.assertEqual(inv_2.residual_signed, 0.0)
+        self.assertEqual(inv_1.amount_residual_signed, 0.0)
+        self.assertEqual(inv_2.amount_residual_signed, 0.0)
