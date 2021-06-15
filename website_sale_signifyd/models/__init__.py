7c3513ef00f12bad8cb15ce908f70cd0f18dfcc2
Jared Kipe <jared@hibou.io>
2021-06-15 10:34:58 -0700

    [IMP] website_sale_signifyd: hibou_professional and improve filter for submitting

--- website_sale_signifyd/models/__init__.py
+++ website_sale_signifyd/models/__init__.py
@@ -1,3 +1,5 @@
+# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
+
 from . import partner
 from . import sale_order
 from . import signifyd
