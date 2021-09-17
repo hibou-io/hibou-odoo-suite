128407aae378c7cb93342eb2d7e881cd30e7358a
Cedric Collins <cedric@hibou.io>
2021-09-17 15:23:06 -0500

    [ADD] base_exception_user,sale_exception_user,stock_exception: allow certain users to ignore exceptions, add exceptions on delivery orders

---
+++ sale_exception_user/wizard/sale_exception_confirm.py
@@ -0,0 +1,16 @@
+from odoo import fields, models
+
+
+class SaleExceptionConfirm(models.TransientModel):
+    _inherit = 'sale.exception.confirm'
+
+    def action_confirm(self):
+        res = super().action_confirm()
+        if self.ignore:
+            self.related_model_id.action_confirm()
+        return res
+
+    def _action_ignore(self):
+        self.related_model_id.ignore_exception = True
+        self.related_model_id.action_confirm()
+        return super()._action_ignore()
