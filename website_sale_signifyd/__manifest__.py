95e74a9dcd88da971d0a8625ff2bab6e9a620ab5
Jared Kipe <jared@hibou.io>
2021-10-06 13:06:22 -0700

    [MIG] website_sale_signifyd: to Odoo 15.0

--- website_sale_signifyd/__manifest__.py
+++ website_sale_signifyd/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Signifyd Connector',
     'author': 'Hibou Corp. <hello@hibou.io>',
-    'version': '14.0.1.0.0',
+    'version': '15.0.1.0.0',
     'category': 'Sale',
     'description': """
 Automate Order Fraud Detection with the Signifyd API.
