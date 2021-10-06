10f7ba474e009a985b1421b54f98e20df05477f9
Jared Kipe <jared@hibou.io>
2021-10-06 07:23:14 -0700

    [MIG] delivery_partner_fedex: to Odoo 15.0

--- delivery_partner_fedex/__manifest__.py
+++ delivery_partner_fedex/__manifest__.py
@@ -1,7 +1,8 @@
 {
     'name': 'Fedex Partner Shipping Accounts',
     'author': 'Hibou Corp. <hello@hibou.io>',
-    'version': '14.0.1.0.0',
+    'version': '15.0.1.0.0',
+    'license': 'LGPL-3',
     'category': 'Stock',
     'sequence': 95,
     'summary': 'Fedex Partner Shipping Accounts',
