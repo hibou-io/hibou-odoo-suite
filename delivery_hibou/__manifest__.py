24c953fb3a8db333c98e12bd97a39c4d63ffe33e
Jared Kipe <jared@hibou.io>
2021-10-06 12:31:03 -0700

    [FIX] delivery_hibou: require professional to be installed, end modules may be paid

--- delivery_hibou/__manifest__.py
+++ delivery_hibou/__manifest__.py
@@ -4,13 +4,14 @@
     'version': '15.0.1.0.0',
     'author': "Hibou Corp.",
     'category': 'Stock',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'images': [],
     'website': "https://hibou.io",
     'description': """
 This is a collection of "typical" carrier needs, and a bridge into Hibou modules like `delivery_partner` and `sale_planner`.
 """,
     'depends': [
+        'hibou_professional',
         'delivery',
         'delivery_partner',
     ],
