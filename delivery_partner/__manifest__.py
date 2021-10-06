a60c989986699b84bb15303094c15c526d83c5a5
Jared Kipe <jared@hibou.io>
2021-10-06 07:02:12 -0700

    [MIG] delivery_partner: to Odoo 15.0

--- delivery_partner/__manifest__.py
+++ delivery_partner/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Partner Shipping Accounts',
     'author': 'Hibou Corp. <hello@hibou.io>',
-    'version': '14.0.1.0.0',
+    'version': '15.0.1.0.0',
     'category': 'Stock',
     'sequence': 95,
     'summary': 'Record shipping account numbers on partners.',
