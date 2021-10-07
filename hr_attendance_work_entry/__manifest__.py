050d07d33153e4eb0f8c3708737e3bc2dc3b6a80
Jared Kipe <jared@hibou.io>
2021-10-07 10:59:27 -0700

    [MIG] hr_attendance_work_entry: to Odoo 15.0

--- hr_attendance_work_entry/__manifest__.py
+++ hr_attendance_work_entry/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Attendance Work Entry Type',
     'description': 'Set work types on attendance records.',
-    'version': '14.0.1.0.0',
+    'version': '15.0.1.0.0',
     'website': 'https://hibou.io/',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'license': 'AGPL-3',
@@ -14,14 +14,20 @@
         'data/hr_attendance_work_entry_data.xml',
         'views/attendance_views.xml',
         'views/employee_views.xml',
-        'views/web_assets.xml',
         'views/work_entry_views.xml',
     ],
     'demo': [
         'data/hr_attendance_work_entry_demo.xml',
     ],
-    'qweb': [
-        'static/src/xml/hr_attendance.xml',
-    ],
+    'assets': {
+        'web.assets_qweb': [
+            'hr_attendance_work_entry/static/src/xml/hr_attendance.xml',
+        ],
+        'web.assets_backend': [
+            'hr_attendance_work_entry/static/src/js/kiosk_confirm.js',
+            'hr_attendance_work_entry/static/src/js/my_attendances.js',
+            'hr_attendance_work_entry/static/src/scss/hr_attendances.scss',
+        ],
+    },
     'pre_init_hook': 'attn_payroll_pre_init_hook',
 }
