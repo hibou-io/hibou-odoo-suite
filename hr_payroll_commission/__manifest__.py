6e574c258a2af9e8cc121427d122e71c3d0a4fc7
Jared Kipe <jared@hibou.io>
2021-10-07 11:59:35 -0700

    [MIG] hr_payroll_commission: to Odoo 15.0

--- hr_payroll_commission/__manifest__.py
+++ hr_payroll_commission/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Commissions in Payslips',
     'author': 'Hibou Corp.',
-    'version': '14.0.1.0.0',
+    'version': '15.0.1.0.0',
     'license': 'OPL-1',
     'category': 'Accounting/Commissions',
     'sequence': 95,
