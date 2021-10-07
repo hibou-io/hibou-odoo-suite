44eb97f053840daaf378b0e1320af7cace7aeb9f
Jared Kipe <jared@hibou.io>
2021-10-07 12:24:54 -0700

    [MIG] auth_admin: to Odoo 15.0

--- auth_admin/__manifest__.py
+++ auth_admin/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Auth Admin',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'category': 'Hidden',
-    'version': '13.0.1.0.0',
+    'version': '15.0.1.0.0',
     'description':
         """
 Login as other user
