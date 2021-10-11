676f7b4120458feadb927e6b5aedc69a1e54442a
Jared Kipe <jared@hibou.io>
2021-10-11 12:55:27 -0700

    [IMP] sale_planner: use multi-rating capability for perf improvement

--- sale_planner/models/stock.py
+++ sale_planner/models/stock.py
@@ -1,3 +1,5 @@
+# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
+
 from odoo import api, fields, models
 
 
@@ -7,3 +9,6 @@ class Warehouse(models.Model):
     shipping_calendar_id = fields.Many2one(
         'resource.calendar', 'Shipping Calendar',
         help="This calendar represents shipping availability from the warehouse.")
+    sale_planner_carrier_ids = fields.Many2many('delivery.carrier',
+                                                string='Sale Order Planner Base Carriers',
+                                                help='Overrides the global carriers.')
