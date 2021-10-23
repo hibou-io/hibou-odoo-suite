6eefda5d825a060e57d75e062ab6cf0598bf026d
Cedric Collins <cedric@hibou.io>
2021-10-22 20:14:00 -0500

    [FIX] website_sale_payment_terms: calculate amounts based on term lines
    add unit tests
    refactor JS widget
    H5924

--- website_sale_payment_terms/models/sale.py
+++ website_sale_payment_terms/models/sale.py
@@ -1,8 +1,22 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+
+    amount_due_today = fields.Float('Due Now', compute='_compute_amount_due_today',
+                                    help='Amount due at checkout on the website.')
+
+    @api.depends('amount_total', 'payment_term_id')
+    def _compute_amount_due_today(self):
+        today_string = fields.Date.to_string(fields.Date.today())
+        for order in self:
+            amount = order.amount_total
+            if order.payment_term_id:
+                term_amount = [amt for date_string, amt in order.payment_term_id.compute(order.amount_total) if date_string == today_string]
+                term_amount = term_amount and term_amount[0] or 0.0
+                amount = term_amount if term_amount > order.amount_total_deposit else order.amount_total_deposit
+            order.amount_due_today = amount
 
     def _check_payment_term_quotation(self, payment_term_id):
         self.ensure_one()
@@ -12,7 +26,4 @@ class SaleOrder(models.Model):
             payment_term = self.env['account.payment.term'].sudo().browse(payment_term_id)
             if not payment_term.exists():
                 raise Exception('Could not find payment terms.')
-            self.write({
-                'payment_term_id': payment_term_id,
-                'require_payment': bool(payment_term.deposit_percentage) or bool(payment_term.deposit_flat),
-            })
+            self.payment_term_id = payment_term
