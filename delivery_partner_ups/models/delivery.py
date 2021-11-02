3bb7c2c6651f3c7ccf45442f35d5a5524682478d
Mishael De La Cruz <mishael@hibou.io>
2021-11-02 09:58:15 -0500

    [I18N] multi: Initial Translations 15.0

--- delivery_partner_ups/models/delivery.py
+++ delivery_partner_ups/models/delivery.py
@@ -1,6 +1,6 @@
 import re
 
-from odoo import fields, models
+from odoo import fields, models, _
 from odoo.exceptions import ValidationError
 
 
@@ -13,7 +13,7 @@ class PartnerShippingAccount(models.Model):
     def ups_check_validity(self):
         m = re.search(r'^[\dA-Z]{6}$', self.name or '')
         if not m:
-            raise ValidationError('UPS Account numbers must be 6 Alpha-numeric characters.')
+            raise ValidationError(_('UPS Account numbers must be 6 Alpha-numeric characters.'))
         m = re.search(r'^\d{5}$', self.ups_zip or '')
         if not m:
-            raise ValidationError('UPS requires the 5 digit account ZIP.')
+            raise ValidationError(_('UPS requires the 5 digit account ZIP.'))
