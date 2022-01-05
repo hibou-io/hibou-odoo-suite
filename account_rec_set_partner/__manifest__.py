33dd813a2f18ec5bb0bdf2b6fd06a4f2029cea4c
Jared Kipe <jared@hibou.io>
2022-01-05 10:51:33 -0800

    [MIG] account_rec_set_partner: to Odoo 15.0

--- account_rec_set_partner/__manifest__.py
+++ account_rec_set_partner/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Reconcile Model Set Partner',
-    'version': '14.0.1.0.0',
+    'version': '15.0.1.0.0',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'category': 'Accounting',
     'summary': 'Reconcile Model can now set the Partner when matched.',
