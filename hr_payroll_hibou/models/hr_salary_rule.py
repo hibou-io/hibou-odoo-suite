13be8c0a15ca7e073c0074b241dbe97ee3643665
Jared Kipe <jared@hibou.io>
2022-01-13 13:36:05 -0800

    [REL] hr_payroll_hibou,l10n_us_hr_payroll,l10n_us_hr_payroll_401k: 2022

--- hr_payroll_hibou/models/hr_salary_rule.py
+++ hr_payroll_hibou/models/hr_salary_rule.py
@@ -17,3 +17,10 @@ class HrPayrollStructureType(models.Model):
     default_schedule_pay = fields.Selection(selection_add=[
         ('semi-monthly', 'Semi-monthly'),
     ])
+    
+
+class HrRuleParameter(models.Model):
+    _inherit = 'hr.rule.parameter'
+
+    update_locked = fields.Boolean(string='Update Lock',
+                                   help='Lock parameter to prevent updating rates from publisher.')
--- l10n_us_hr_payroll_401k/tests/__init__.py
+++ l10n_us_hr_payroll_401k/tests/__init__.py
@@ -1,3 +1,3 @@
 # Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
 
-from . import test_payroll
+# Moved to l10n_us_hr_payroll_401k_params
