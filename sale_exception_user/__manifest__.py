c09616259b0bcf1beca24692e22d6e078a09c3b0
Cedric Collins <cedric@hibou.io>
2022-01-14 19:21:57 -0600

    [MIG] sale_exception_user: bump version and add unit tests

--- sale_exception_user/__manifest__.py
+++ sale_exception_user/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Sale Exception Rule User',
-    'version': '13.0.1.0.0',
+    'version': '14.0.1.0.0',
     'author': 'Hibou Corp.',
     'license': 'OPL-1',
     'category': 'Generic Modules',
