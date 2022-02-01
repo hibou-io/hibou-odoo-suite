f9552ed31b6f18657460f55225ed2938dbf61650
Jared Kipe <jared@hibou.io>
2022-02-01 15:17:39 -0800

    [MIG] hr_commission_timesheet: to 15.0

--- hr_commission_timesheet/__manifest__.py
+++ hr_commission_timesheet/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Timesheet Commissions',
     'description': 'Pay commission on billed timesheets.',
-    'version': '14.0.1.0.0',
+    'version': '15.0.1.0.0',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'depends': [
         'hr_commission',
