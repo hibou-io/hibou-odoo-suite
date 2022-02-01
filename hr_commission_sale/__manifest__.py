c8f60544c7471d960a1f1b1d8e2877b6f9e5180e
Jared Kipe <jared@hibou.io>
2022-02-01 14:46:36 -0800

    [MIG] hr_commission_sale: to 14.0

--- hr_commission_sale/__manifest__.py
+++ hr_commission_sale/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Sale Commissions',
     'description': 'Bridge module to add commission menus for sales users.',
-    'version': '13.0.1.0.0',
+    'version': '14.0.1.0.0',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'depends': [
         'hr_commission',
