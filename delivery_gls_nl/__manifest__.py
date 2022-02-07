663c57c9b7fd087d143bc5cd639e3fa905c9a4f2
Jared Kipe <jared@hibou.io>
2022-02-07 08:39:41 -0800

    [MIG] delivery_gls_nl: to 15.0

--- delivery_gls_nl/__manifest__.py
+++ delivery_gls_nl/__manifest__.py
@@ -1,10 +1,12 @@
+# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
+
 {
     'name': 'GLS Netherlands Shipping',
     'summary': 'Create and print your shipping labels with GLS from the Netherlands.',
-    'version': '12.0.1.0.0',
+    'version': '15.0.1.0.0',
     'author': "Hibou Corp.",
     'category': 'Warehouse',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'images': [],
     'website': "https://hibou.io",
     'description': """
--- delivery_gls_nl/models/gls_nl_request.py
+++ delivery_gls_nl/models/gls_nl_request.py
@@ -1,3 +1,5 @@
+# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
+
 import requests
 from json import dumps
 
