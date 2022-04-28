88957a77410b88b6ad73bbecc1b519bc39a90e08
Jared Kipe <jared@hibou.io>
2022-04-28 19:26:28 +0000

    [IMP] l10n_pe_hr_payroll: move tests and improve

--- l10n_pe_hr_payroll/models/__init__.py
+++ l10n_pe_hr_payroll/models/__init__.py
@@ -1,6 +1,5 @@
 # Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
 
-from . import browsable_object
 from . import hr_contract
 from . import hr_payslip
 from . import pe_payroll_config
--- l10n_pe_hr_payroll/tests/__init__.py
+++ l10n_pe_hr_payroll/tests/__init__.py
@@ -1,9 +1,6 @@
 # Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
 
-# TODO make/move to l10n_pe_hr_payroll_params
-# Tests moved to `l10n_us_hr_payroll_params`
+# Tests moved to `l10n_pe_hr_payroll_params`
 # common remains for site specific tests
 
 from . import common
-from . import test_2020
-# from . import test_2022
