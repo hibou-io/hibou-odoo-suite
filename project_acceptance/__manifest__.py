04cfe42d5596f645b880e3e7abe0f56dc9ff4d2f
Leo Pinedo <leo@hibou.pe>
2022-09-28 23:54:49 +0000

    [WIP] project_acceptance:created email template, added fields and buttons
    H11043

---
+++ project_acceptance/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Project Acceptance',
+    'version': '15.0.1.0.0',
+    'author': 'Hibou Corp. <hello@hibou.io>',
+    'website': 'https://hibou.io/',
+    'license': 'AGPL-3',
+    'category': 'Tools',
+    'complexity': 'easy',
+    'description': """    """,
+    'depends': [
+        'project',
+    ],
+    'data': [
+        'data/mail_template_data.xml',
+        'views/project_portal_templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
