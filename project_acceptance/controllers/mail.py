1cf7cf38dc6dc74ec54a2a7857fa9a81b9ecde3c
Leo Pinedo <leo@hibou.pe>
2022-10-05 16:00:22 +0000

    [WIP] project_acceptance: change on controllers for exception rule
    H11043

--- project_acceptance/controllers/mail.py
+++ project_acceptance/controllers/mail.py
@@ -11,6 +11,8 @@ class PortalChatter(mail.PortalChatter):
     def portal_chatter_post(self, res_model, res_id, message, attachment_ids='', attachment_tokens='', **kwargs):
         if request.httprequest.method == 'POST':
             task = request.env['project.task'].browse([res_id])
-            task.task_acceptance = 'feedback'
+            task.sudo().ignore_exception = True
+            task.sudo().task_acceptance = 'feedback'
+            task.sudo().ignore_exception = False
         return super(PortalChatter, self).portal_chatter_post(res_model, res_id, message, attachment_ids=attachment_ids, attachment_tokens=attachment_tokens, **kwargs)
     
