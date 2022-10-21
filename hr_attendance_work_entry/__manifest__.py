ea323abef624ffc717d035bf5296ac6e3d338d21
Leo Pinedo <leo@hibou.pe>
2022-10-21 20:07:01 +0000

    [MIG] hr_attendance_work_entry: to 16

--- hr_attendance_work_entry/__manifest__.py
+++ hr_attendance_work_entry/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Attendance Work Entry Type',
     'description': 'Set work types on attendance records.',
-    'version': '15.0.1.0.0',
+    'version': '16.0.1.0.0',
     'website': 'https://hibou.io/',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'license': 'AGPL-3',
