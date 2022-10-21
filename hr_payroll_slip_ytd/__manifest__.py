790be6186f065dbae958355f70cf785a3bd7a310
Leo Pinedo <leo@hibou.pe>
2022-10-21 22:16:36 +0000

    [MIG] hr_payroll_slip_ytd: To 16 error on get amout payslip

--- hr_payroll_slip_ytd/__manifest__.py
+++ hr_payroll_slip_ytd/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Payroll Report Year to Date',
     'author': 'Hibou Corp. <hello@hibou.io>',
-    'version': '15.0.1.0.0',
+    'version': '16.0.1.0.0',
     'category': 'Human Resources',
     'sequence': 95,
     'summary': 'Show YTD computations on Payslip Report',
