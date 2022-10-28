2a80af849c24172478ab659c8aab44e05c848f17
Leo Pinedo <leo@hibou.pe>
2022-10-28 22:51:54 +0000

    [MIG] delivery_gls_nl: to v16

--- delivery_gls_nl/__manifest__.py
+++ delivery_gls_nl/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'GLS Netherlands Shipping',
     'summary': 'Create and print your shipping labels with GLS from the Netherlands.',
-    'version': '15.0.1.0.0',
+    'version': '16.0.1.0.0',
     'author': "Hibou Corp.",
     'category': 'Warehouse',
     'license': 'LGPL-3',
