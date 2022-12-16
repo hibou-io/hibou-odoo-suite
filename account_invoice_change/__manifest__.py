b441f3448d1e4ca0ebcda57e910149d6443114f6
Dan Tillinghast <dan@compassfoundation.io>
2022-12-16 11:11:19 -0500

    [MIG] account_invoice_change: Migration to 14.0

--- account_invoice_change/__manifest__.py
+++ account_invoice_change/__manifest__.py
@@ -1,20 +1,20 @@
 {
     'name': 'Account Invoice Change',
     'author': 'Hibou Corp. <hello@hibou.io>',
-    'version': '13.0.1.0.0',
+    'version': '14.0.1.0.0',
     'category': 'Accounting',
     'sequence': 95,
     'summary': 'Technical foundation for changing invoices.',
     'description': """
 Technical foundation for changing invoices.
 
-Creates wizard and permissions for making invoice changes that can be 
+Creates wizard and permissions for making invoice changes that can be
 handled by other individual modules.
 
 This module implements, as examples, how to change the Salesperson and Date fields.
 
-Abstractly, individual 'changes' should come from specific 'fields' or capability 
-modules that handle the consequences of changing that field in whatever state the 
+Abstractly, individual 'changes' should come from specific 'fields' or capability
+modules that handle the consequences of changing that field in whatever state the
 the invoice is currently in.
 
     """,
@@ -24,6 +24,7 @@ the invoice is currently in.
     ],
     'data': [
         'wizard/invoice_change_views.xml',
+        'security/ir.model.access.csv',
     ],
     'installable': True,
     'application': False,
