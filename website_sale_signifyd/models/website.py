f439b816fba579338a8fe231f11cfd3d558d2df7
Cedric Collins <cedric@hibou.io>
2023-01-25 13:06:24 -0600

    [REM] website_sale_signifyd: move to professional

--- website_sale_signifyd/models/website.py
+++
@@ -1,9 +0,0 @@
-# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
-
-from odoo import api, fields, models
-
-
-class Website(models.Model):
-    _inherit = 'website'
-
-    signifyd_connector_id = fields.Many2one('signifyd.connector', ondelete='set null')
--- website_sale_signifyd/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import test_order_flow
