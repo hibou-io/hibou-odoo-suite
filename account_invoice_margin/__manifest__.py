eb4dd60f065da4f54685b200e48d1fe59247aaa6
Jared Kipe <jared@hibou.io>
2023-04-18 22:14:26 +0000

    [MIG] account_invoice_margin: to 16.0
    (from 15.0 hard copy due to errors)

---
+++ account_invoice_margin/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Invoice Margin',
+    'author': 'Hibou Corp. <hello@hibou.io>',
+    'version': '16.0.1.0.0',
+    'license': 'AGPL-3',
+    'category': 'Accounting',
+    'sequence': 95,
+    'summary': 'Invoices include margin calculation.',
+    'description': """
+Invoices include margin calculation.
+If the invoice line comes from a sale order line, the cost will come 
+from the sale order line.
+    """,
+    'website': 'https://hibou.io/',
+    'depends': [
+        'account',
+        'sale_margin',
+    ],
+    'data': [
+        'views/account_invoice_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
