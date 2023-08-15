4c00222a9a6bb1143c4af3c672187a141fc8da1c
Salomon Chambi <salomon@hibou.pe>
2023-08-15 21:28:24 +0000

    [MIG] website_project_task: to v16

--- website_project_task/__manifest__.py
+++ website_project_task/__manifest__.py
@@ -1,11 +1,12 @@
 {
     'name': 'Website Project Tasks',
-    'version': '15.0.1.0.0',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'website': 'https://hibou.io/',
+    'version': '16.0.1.0.0',
     'license': 'AGPL-3',
     'category': 'Tools',
     'complexity': 'easy',
+    'summary': 'Adds tags on tasks, classes on stages and tags for CSS hooks in Website Project.',
     'description': """
 This module adds options to Website Project:
 ============================================
