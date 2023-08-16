dbf656022562b8f4a6d6163b37b919f28eb8f7dc
Juan Pablo Ticse <juanpablo@hibou.pe>
2023-08-16 20:04:54 +0000

    [MIG] sale_exception_website: Updated manifest and test GUI

--- sale_exception_website/__manifest__.py
+++ sale_exception_website/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Sale Exception Website',
     'summary': 'Display sale exceptions on eCommerce site',
-    'version': '15.0.1.0.0',
+    'version': '16.0.1.0.0',
     'author': "Hibou Corp.",
     'category': 'Sale',
     'license': 'AGPL-3',
