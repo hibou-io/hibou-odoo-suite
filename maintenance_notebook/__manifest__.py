1141bafedeec9e4dccfff1857a797f6962e7fb2e
Juan Pablo Ticse <juanpablo@hibou.pe>
2023-10-31 22:19:19 +0000

    [MIG] maintenance_notebook: module migration

--- maintenance_notebook/__manifest__.py
+++ maintenance_notebook/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Maintenance Notebook',
-    'version': '16.0.1.0.0',
+    'version': '17.0.1.0.0',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'category': 'Human Resources',
     'summary': 'Record time on maintenance requests.',
