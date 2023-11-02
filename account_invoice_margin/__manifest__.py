c017c1b40b67e90d52fa9b95b770d30f1c412475
Salomon Chambi <salomon@hibou.pe>
2023-11-02 22:16:26 +0000

    [MIG] account_invoice_margin: to 17.0

--- account_invoice_margin/__manifest__.py
+++ account_invoice_margin/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Invoice Margin',
-    'author': 'Hibou Corp. <hello@hibou.io>',
-    'version': '16.0.1.1.0',
+    'version': '17.0.1.1.0',
+    'author': 'Hibou Corp.',
     'license': 'AGPL-3',
     'category': 'Accounting',
     'sequence': 95,
