dd2c57a9b36f5c512991ad4d271f5e1079e91d24
Salomon Chambi <salomon@hibou.pe>
2023-11-03 20:37:43 +0000

    [MIG] delivery_partner_ups: to 17.0

--- delivery_partner_ups/__manifest__.py
+++ delivery_partner_ups/__manifest__.py
@@ -1,12 +1,17 @@
 {
     'name': 'UPS Partner Shipping Accounts',
-    'author': 'Hibou Corp. <hello@hibou.io>',
-    'version': '16.0.1.0.0',
+    'author': 'Hibou Corp.',
+    'version': '17.0.1.0.0',
     'license': 'LGPL-3',
     'category': 'Stock',
     'sequence': 95,
     'summary': 'UPS Partner Shipping Accounts',
     'description': """
+UPS Partner Shipping Accounts
+=============================
+This module adds UPS to the delivery type selection dropdown on the Partner Shipping Account model.
+Additionally, it adds a new required field UPS Account ZIP, as well as validation of entered UPS account number.
+
     """,
     'website': 'https://hibou.io/',
     'depends': [
