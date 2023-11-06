7ed3ecf1f905cf0d5170ea9d54661586a4b0c6e1
Salomon Chambi <salomon@hibou.pe>
2023-11-06 22:06:11 +0000

    [MIG] delivery_partner_fedex: to 17.0

--- delivery_partner_fedex/__manifest__.py
+++ delivery_partner_fedex/__manifest__.py
@@ -1,12 +1,17 @@
 {
     'name': 'Fedex Partner Shipping Accounts',
-    'author': 'Hibou Corp. <hello@hibou.io>',
-    'version': '16.0.1.0.0',
+    'author': 'Hibou Corp.',
+    'version': '17.0.1.0.0',
     'license': 'LGPL-3',
     'category': 'Stock',
     'sequence': 95,
     'summary': 'Fedex Partner Shipping Accounts',
     'description': """
+Fedex Partner Shipping Accounts
+===============================
+This module adds FedEx to delivery type selection dropdown on the Partner Shipping Account model.
+Additionally, it validates entered FedEx account numbers are the correct length.
+
     """,
     'website': 'https://hibou.io/',
     'depends': [
