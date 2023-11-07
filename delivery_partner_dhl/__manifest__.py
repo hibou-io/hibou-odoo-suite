1f9249d742b82694d9d605975661364203965b77
Salomon Chambi <salomon@hibou.pe>
2023-11-07 23:36:10 +0000

    [MIG] delivery_partner_dhl: to 17.0

--- delivery_partner_dhl/__manifest__.py
+++ delivery_partner_dhl/__manifest__.py
@@ -1,12 +1,17 @@
 {
     'name': 'DHL Partner Shipping Accounts',
-    'author': 'Hibou Corp. <hello@hibou.io>',
-    'version': '16.0.1.0.0',
+    'author': 'Hibou Corp.',
+    'version': '17.0.1.0.0',
     'license': 'LGPL-3',
     'category': 'Stock',
     'sequence': 95,
     'summary': 'DHL Partner Shipping Accounts',
     'description': """
+DHL Partner Shipping Accounts
+===============================
+This module adds DHL to delivery type selection dropdown on the Partner Shipping Account model.
+Additionally, it validates entered DHL account numbers are the correct length.
+
     """,
     'website': 'https://hibou.io/',
     'depends': [
