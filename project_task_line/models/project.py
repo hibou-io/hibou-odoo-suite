e5c387e99e4934e8edf09d548f71b04c7d087d31
Cedric Collins <cedric@hibou.io>
2023-11-13 18:30:31 -0600

    [MIG] project_task_line: 17.0

--- project_task_line/models/project.py
+++ project_task_line/models/project.py
@@ -5,32 +5,6 @@ class ProjectTask(models.Model):
     _inherit = 'project.task'
 
     line_ids = fields.One2many('project.task.line', 'task_id', string='Todo List')
-    subtask_count_done = fields.Integer(compute='_compute_subtask_count', string="Sub-task Done count")
-
-    @api.depends('child_ids')
-    def _compute_subtask_count(self):
-        for task in self:
-            subtasks = task._get_all_subtasks()
-            task.subtask_count = len(subtasks)
-            task.subtask_count_done = len(subtasks.filtered(lambda t: t.is_closed))
-    
-    def action_subtask(self):
-        action = self.env.ref('project.action_view_all_task').sudo().read()[0]
-
-        # display all subtasks of current task
-        action['domain'] = [('id', 'child_of', self.id), ('id', '!=', self.id)]
-
-        ctx = dict(self.env.context)
-        ctx = {k: v for k, v in ctx.items() if not k.startswith('search_default_')}
-        ctx.update({
-            'default_name': self.env.context.get('name', self.name) + ':',
-            'default_parent_id': self.id,  # will give default subtask field in `default_get`
-            'default_company_id': self.env.company.id,
-        })
-
-        action['context'] = ctx
-
-        return action
 
 
 class ProjectTaskLine(models.Model):
@@ -40,19 +14,19 @@ class ProjectTaskLine(models.Model):
 
     task_id = fields.Many2one('project.task', required=True)
     name = fields.Char(string='Name')
-    user_id = fields.Many2one('res.users', string='User')
+    user_id = fields.Many2one(
+        'res.users', string='Completed By', 
+        context={'active_test': False}, 
+        compute='_compute_user_id', 
+        store=True, readonly=False, precompute=True,
+    )
     sequence = fields.Integer(string='Sequence')
-    kanban_state = fields.Selection([
-        ('normal', ''),
+    state = fields.Selection([
         ('done', 'Done'),
-        ('blocked', 'Blocked')], string='State',
-        copy=False, default='normal', required=True,
-        help="A task's kanban state indicates special situations affecting it:\n"
-             " * Blank is the default situation\n"
-             " * Blocked indicates something is preventing the progress of this task\n"
-             " * Done indicates the task is complete")
+        ('blocked', 'Blocked'),
+    ], string='State', copy=False)
 
-    @api.onchange('kanban_state')
-    def _onchange_kanban_state(self):
-        if self.kanban_state == 'done':
-            self.user_id = self.env.user
+    @api.depends('state')
+    def _compute_user_id(self):
+        for line in self.filtered(lambda l: l.state == 'done' and not l.user_id):
+            line.user_id = self.env.user
