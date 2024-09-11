73e2f144e53fa248c6d5b7ba450788dae6fbb2bf
Mayank Patel <mayank@hibou.io>
2024-09-11 05:45:08 +0000

    [REM] delivery_purolator: available in professional
    H14528

--- delivery_purolator/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import delivery_purolator
-from . import stock_package_type
--- delivery_purolator/models/stock_package_type.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class PackageType(models.Model):
-    _inherit = 'stock.package.type'
-
-    package_carrier_type = fields.Selection(selection_add=[('purolator', 'Purolator')])
