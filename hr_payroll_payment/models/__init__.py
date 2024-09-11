9053e1677c86dcf8219bb63ab95d549f3ad0c4b4
Mayank Patel <mayank@hibou.io>
2024-09-11 05:47:51 +0000

    [REM] hr_payroll_payment: available in professional
    H14528

--- hr_payroll_payment/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
-
-from . import account
-from . import hr_payslip
-from . import hr_payslip_patch
