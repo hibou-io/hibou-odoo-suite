40415acaf49f37076962ee186e9de1f143a03eb3
Mayank Patel <mayank@hibou.io>
2024-09-11 05:49:40 +0000

    [REM] rma: available in professional
    H14528

--- rma/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
-
-from . import account
-from . import rma
-from . import stock_picking
--- rma/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
-
-from . import rma_lines
-from . import rma_make_rtv
