94903f013222120f7c7788462fcd8905de12c5b9
Mayank Patel <mayank@hibou.io>
2024-09-11 05:47:37 +0000

    [REM] hr_payroll_hibou: available in professional
    H14528

--- hr_payroll_hibou/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import browsable_object
-from . import hr_contract
-from . import hr_payslip
-from . import hr_salary_rule
-from . import res_config_settings
-from . import update
--- hr_payroll_hibou/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
-
-from . import common
-
-from . import test_contract_wage_type
-from . import test_special
-from . import test_update
