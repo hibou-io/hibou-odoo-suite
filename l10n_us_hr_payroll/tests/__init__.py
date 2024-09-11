b5a8ea87eb2d3d591965010d8f72a224153f8627
Mayank Patel <mayank@hibou.io>
2024-09-11 05:48:42 +0000

    [REM] l10n_us_hr_payroll: available in professional
    H14528

--- l10n_us_hr_payroll/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
-
-# Tests moved to `l10n_us_hr_payroll_params`
-# common remains for site specific tests
-
-from . import common
