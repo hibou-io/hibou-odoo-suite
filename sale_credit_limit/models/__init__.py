7edb4859eae38c34836ef4d01ac85580956b8696
Mayank Patel <mayank@hibou.io>
2024-09-11 05:50:04 +0000

    [REM] sale_credit_limit: available in professional
    H14528

--- sale_credit_limit/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
-
-from . import partner
-from . import sale
--- sale_credit_limit/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
-
-from . import test_sale_credit_exception
