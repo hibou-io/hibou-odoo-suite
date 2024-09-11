0a9992ab66a7e3f74e9d9fc7c57cf8ecde559a48
Mayank Patel <mayank@hibou.io>
2024-09-11 05:49:14 +0000

    [REM] product_cores: available in professional
    H14528

--- product_cores/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
-
-from . import account
-from . import product
-from . import purchase
-from . import sale
