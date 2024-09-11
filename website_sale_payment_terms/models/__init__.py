4896cbc9c810a73b8cddc82bb4d3fc4fac1861a9
Mayank Patel <mayank@hibou.io>
2024-09-11 05:51:31 +0000

    [REM] website_sale_payment_terms: available in professional
    H14528

--- website_sale_payment_terms/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import account
-from . import payment
-from . import res_config
-from . import sale
-from . import website
--- website_sale_payment_terms/models/account.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class AccountPaymentTerm(models.Model):
-    _inherit = 'account.payment.term'
-
-    allow_in_website_sale = fields.Boolean('Allow in website checkout')
