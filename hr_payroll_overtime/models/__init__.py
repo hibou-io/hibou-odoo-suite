eb02b8e6b95c40a9c83ad1b11d213a919327bb7f
Mayank Patel <mayank@hibou.io>
2024-09-11 05:47:45 +0000

    [REM] hr_payroll_overtime: available in professional
    H14528

--- hr_payroll_overtime/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import hr_contract
-from . import hr_payslip
-from . import hr_work_entry
-from . import resource_calendar
--- hr_payroll_overtime/models/hr_contract.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class HrContract(models.Model):
-    _inherit = 'hr.contract'
-
-    is_overtime_exempt = fields.Boolean(string='Overtime Exempt',
-                                        help='e.g. Agriculture or farm work exempt under the US FLSA.')
