ba5df7a8a8763273c49c3b2042a6059830eebff1
Mayank Patel <mayank@hibou.io>
2024-09-11 05:45:41 +0000

    [REM] delivery_ups_hibou: available in professional
    H14528

--- delivery_ups_hibou/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
-
-from . import delivery_ups
-from . import stock
-from . import ups_request_patch
--- delivery_ups_hibou/models/stock.py
+++
@@ -1,9 +0,0 @@
-# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
-
-from odoo import fields, models
-
-
-class StockWarehouse(models.Model):
-    _inherit = 'stock.warehouse'
-
-    ups_shipper_number = fields.Char(string='UPS Shipper Number')
