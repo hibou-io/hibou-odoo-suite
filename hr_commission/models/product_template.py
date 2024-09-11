eff5b54e0d598519af8df9e33f374e5f5996154f
Mayank Patel <mayank@hibou.io>
2024-09-11 05:46:51 +0000

    [REM] hr_commission: available in professional
    H14528

--- hr_commission/models/product_template.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    is_commission_exempt = fields.Boolean('Exclude from Commissions')
--- hr_commission/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
-
-from . import test_commission
-from . import test_is_commission_exempt
