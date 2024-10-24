4a6a4366651ef734bfd9bfe43a9781039bc7b8ca
Mayank Patel <mayank@hibou.io>
2024-10-24 10:43:22 +0000

    [MIG] hr_timesheet_work_entry: Migrated to 18.0
    H14631

--- hr_timesheet_work_entry/__manifest__.py
+++ hr_timesheet_work_entry/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Timesheet Work Entry Type',
     'description': 'Set work types on timesheet records.',
-    'version': '17.0.1.0.0',
+    'version': '18.0.1.0.0',
     'website': 'https://hibou.io/',
     'author': 'Hibou Corp.',
     'license': 'AGPL-3',
