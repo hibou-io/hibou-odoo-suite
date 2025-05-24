e2c8eef2c6f589f140d38d781b08c42baa744402
Jared Kipe <jared@hibou.io>
2025-05-24 22:28:25 +0000

    [MIG] website_project_task: to 18.0

--- website_project_task/__manifest__.py
+++ website_project_task/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Website Project Tasks',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'website': 'https://hibou.io/',
-    'version': '17.0.1.0.0',
+    'version': '18.0.1.0.0',
     'license': 'AGPL-3',
     'category': 'Tools',
     'complexity': 'easy',
