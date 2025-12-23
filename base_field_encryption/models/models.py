3d8eff53cba8098bd3f5c398f1160125d0dbf9bd
Mayank Patel <mayank@hibou.io>
2025-12-23 12:57:06 +0000

    [MIG] base_field_encryption
    H16569

---
+++ base_field_encryption/models/models.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Hibou Suite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
+
+from odoo import models, fields
+
+ENCRYYPTION_SUPPORTED_FIELD_TYPES = ["char", "text", "html", "selection"]
+
+
+class Base(models.AbstractModel):
+    _inherit = 'base'
+
+    def _valid_field_parameter(self, field, name):
+        return (name == 'encrypt' and field.type in ENCRYYPTION_SUPPORTED_FIELD_TYPES) or super()._valid_field_parameter(field, name)
+
+
+class IrModelFields(models.Model):
+    _inherit = 'ir.model.fields'
+
+    ttype = fields.Selection(selection_add=[('encryption', 'encryption')], ondelete={'encryption': 'cascade'})
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    encrypt = fields.Encryption()
+    test = fields.Char(encrypt="encrypt")
