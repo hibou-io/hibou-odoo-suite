d036ba4bf4d89825d9340a729a20c43510db4533
Milan <milan@hibou.io>
2026-01-02 23:21:49 +0100

    [ADD] web_request_blocker: user agent exclusion H16568
    -----
    [ADD] web_request_blocker: user agent exclusion regexp
    Initial version configured through ir.config_parameter only
    
    [IMP] web_request_blocker: parameter validation
    h16568
    
    [FIX] web_request_blocker: correct license
    h16568
    
    [FIX] web_request_blocker: check for request before fetching user agent
    h16568
    
    [IMP] web_request_blocker: settings through odoo.conf / environment variables
    h16568
    
    [FIX] web_request_blocker: ICP value constraint request check
    h16568
    
    [ADD] web_request_blocker: backport to 18.0
    
    [MIG] web_request_blocker: backport from 18.0
    
    [MIG] web_request_blocker: backport from 17.0
    
    [MIG] web_request_blocker: backport from 16.0
    
    [MIG] web_request_blocker: backport to 14.0
    
    [MIG] web_request_blocker: backport from 14.0

---
+++ web_request_blocker/models/ir_http.py
@@ -0,0 +1,30 @@
+from odoo import models
+from odoo.http import request
+from werkzeug.exceptions import Forbidden
+import re
+
+
+KEY = 'web_request_blocker.user_agent_pattern'
+FORCE_KEY = 'web_request_blocker.force_user_agent_pattern'
+
+from odoo.tools import config
+from os import environ
+# Can't user config._env_options for undeclared option
+
+EXTERNAL_PATTERN = environ.get(KEY) or config.options.get(KEY)
+IGNORE_ICP = environ.get(FORCE_KEY) or config.options.get(FORCE_KEY)
+
+
+class IrHttp(models.AbstractModel):
+    _inherit = 'ir.http'
+
+    @classmethod
+    def _match(cls, path_info, key=None):
+        # re and _get_param both handle their own caching!
+        ICP = request.env['ir.config_parameter'].sudo()
+        # Skipping get_params's access rights check
+        pattern = (not IGNORE_ICP and ICP._get_param(KEY)) or EXTERNAL_PATTERN
+        user_agent = request and request.httprequest.environ.get('HTTP_USER_AGENT')
+        if pattern and user_agent and re.match(pattern, user_agent):
+            raise Forbidden()
+        return super()._match(path_info, key)
